7479f1040f41c23e4eca1f84d8199a7e98fd5e0b
Carlos Mocholí <carlossmocholi@gmail.com>
2023-03-22 20:17:22 -0400

    srv/data

--- generate.py
+++ generate.py
@@ -15,14 +15,14 @@ def generate(
     L.seed_everything(1234)
 
     device = torch.device("cuda")
-    checkpoint = torch.load("./data/checkpoints/llama/converted_meta/7B/state_dict.pt")
+    checkpoint = torch.load("/srv/data/checkpoints/llama/converted_meta/7B/state_dict.pt")
 
     with device:
         model = LLAMA(LLAMA_CONFIG_DICT["7B"])
         model.load_state_dict(checkpoint)
     model.eval()
 
-    tokenizer = Tokenizer("./data/checkpoints/llama/converted_meta/tokenizer.model")
+    tokenizer = Tokenizer("/srv/data/checkpoints/llama/converted_meta/tokenizer.model")
     encoded_prompt = tokenizer.encode(prompt, bos=True, eos=False).to(device)
     encoded_prompt = encoded_prompt[None, :]
     for k in range(num_samples):
