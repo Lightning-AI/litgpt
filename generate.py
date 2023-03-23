ddfa90c1ed7e7c3711312d556462c08d602b2c97
Carlos Mocholí <carlossmocholi@gmail.com>
2023-03-22 20:28:45 -0400

    `generate` docstring

--- generate.py
+++ generate.py
@@ -11,7 +11,19 @@ def generate(
     steps: int = 20,
     top_k: int = 200,
     temperature: float = 0.8,
+    compile: bool = False,
 ):
+    """
+    Generates text samples based on a pre-trained LLaMA model and tokenizer.
+
+    Args:
+        prompt: The prompt string to use for generating the samples.
+        num_samples: The number of text samples to generate.
+        steps: The number of generation steps to take.
+        top_k: The number of top most probable tokens to consider in the sampling process.
+        temperature: A value controlling the randomness of the sampling process. Higher values result in more random samples.
+        compile: Whether to compile the model.
+    """
     L.seed_everything(1234)
 
     device = torch.device("cuda")
@@ -21,6 +33,9 @@ def generate(
         model = LLAMA(LLAMA_CONFIG_DICT["7B"])
         model.load_state_dict(checkpoint)
     model.eval()
+
+    if compile:
+        model = torch.compile(model)
 
     tokenizer = Tokenizer("/srv/data/checkpoints/llama/converted_meta/tokenizer.model")
     encoded_prompt = tokenizer.encode(prompt, bos=True, eos=False).to(device)
