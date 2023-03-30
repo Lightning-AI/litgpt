139e6a85ace67462d92edc7cc2d1839a8f702d57
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-03-30 09:18:55 +0200

    fix `setup.py` (#48)

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
+    readme = fo.read()
 
 setup(
     name='lit-llama',
@@ -13,4 +21,6 @@ setup(
         "bitsandbytes",
     ],
     packages=find_packages(),
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
