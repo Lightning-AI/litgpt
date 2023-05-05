f1ce1ea3f2832b8de9d64c27b4e8f9f38a469bfa
Carlos Mocholí <carlossmocholi@gmail.com>
2023-05-05 18:25:33 +0200

    Import error

--- lit_stablelm/__init__.py
+++ lit_stablelm/__init__.py
@@ -1,2 +1,10 @@
 from lit_stablelm.model import StableLMConfig, StableLM, build_rope_cache, apply_rope
 from lit_stablelm.tokenizer import Tokenizer
+
+from lightning_utilities.core.imports import RequirementCache
+
+if not bool(RequirementCache("torch>=2.1.0dev")):
+    raise ImportError(
+        "Lit-StableLM requires torch nightly (future torch 2.1). Please follow the installation instructions in the"
+        " repository README.md"
+    )
