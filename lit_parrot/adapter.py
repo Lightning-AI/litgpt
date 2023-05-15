525b88d63d23f1b2aa04aecbb781d24ce3e05a59
Artur Komenda <62992414+ArturK-85@users.noreply.github.com>
2023-05-15 18:22:40 +0200

    Apply proposed changes from issues #58 #57

--- lit_parrot/adapter.py
+++ lit_parrot/adapter.py
@@ -19,6 +19,9 @@ from lit_parrot.config import Config as BaseConfig
 from lit_parrot.model import MLP, Parrot as BaseModel, build_rope_cache, apply_rope
 
 
+RoPECache = Tuple[torch.Tensor, torch.Tensor]
+KVCache = Tuple[torch.Tensor, torch.Tensor]
+
 @dataclass
 class Config(BaseConfig):
     adapter_prompt_length: int = 10
@@ -53,7 +56,16 @@ class CausalSelfAttention(nn.Module):
         self.adapter_prompt_length = config.adapter_prompt_length
         self.adapter_start_layer = config.adapter_start_layer
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        rope: RoPECache,
+        mask: torch.Tensor,
+        max_seq_length: int,
+        input_pos: Optional[torch.Tensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> Tuple[torch.Tensor, Optional[KVCache]]:
         B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
 
         qkv = self.attn(x)
@@ -62,39 +74,37 @@ class CausalSelfAttention(nn.Module):
         q, k, v = qkv.split(head_size, dim=-1)  # (B, nh, T, hs)
 
         n_elem = int(self.rotary_percentage * head_size)
-        if self.rope_cache is None:
-            self.rope_cache = build_rope_cache(self.block_size, n_elem, x.dtype, x.device)
-        cos, sin = self.rope_cache
-        cos, sin = cos[:T], sin[:T]
 
+        cos, sin = rope
         q_roped = apply_rope(q[..., :n_elem], cos, sin)
         k_roped = apply_rope(k[..., :n_elem], cos, sin)
         q = torch.cat((q_roped, q[..., n_elem:]), dim=-1)
         k = torch.cat((k_roped, k[..., n_elem:]), dim=-1)
 
+        if kv_cache is not None:
+            cache_k, cache_v = kv_cache
+            cache_k = cache_k.type(torch.bfloat16)
+            cache_v = cache_v.type(torch.bfloat16)
+            # check if reached token limit
+            if input_pos[-1] >= max_seq_length:
+                input_pos = torch.tensor(max_seq_length - 1, device=input_pos.device)
+                # shift 1 position to the left
+                cache_k = torch.roll(cache_k, -1, dims=2)
+                cache_v = torch.roll(cache_v, -1, dims=2)
+            k = cache_k.index_copy(2, input_pos, k)
+            v = cache_v.index_copy(2, input_pos, v)
+            kv_cache = k, v
+
         # efficient attention using Flash Attention CUDA kernels
-        y = F.scaled_dot_product_attention(
-            q, k, v, attn_mask=None, dropout_p=0.0, is_causal=True, scale=1.0 / math.sqrt(head_size)
-        )
-
-        if self.block_idx >= self.adapter_start_layer:
-            prefix = self.adapter_wte.weight.reshape(1, self.adapter_prompt_length, self.n_embd)
-
-            aT = prefix.size(1)
-            _, ak, av = self.attn(prefix).split(self.n_embd, dim=2)  # mayby dim=2
-            ak = ak.view(1, aT, self.n_head, head_size).repeat(B, 1, 1, 1).transpose(1, 2)
-            av = av.view(1, aT, self.n_head, head_size).repeat(B, 1, 1, 1).transpose(1, 2)
-
-            amask = torch.ones(q.shape[-2], ak.shape[-2], dtype=torch.bool, device=x.device)
-            ay = F.scaled_dot_product_attention(q, ak, av, attn_mask=amask, dropout_p=0.0, is_causal=False)
-            y = y + self.gating_factor * ay
+        y = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0, scale=1.0 / math.sqrt(head_size))
 
         y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
 
         # output projection
         y = self.proj(y)
 
-        return y
+        return y, kv_cache
+        
 
 
 class Block(nn.Module):
@@ -110,13 +120,22 @@ class Block(nn.Module):
 
         self.parallel_residual = config.parallel_residual
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self,
+        x: torch.Tensor,
+        rope: RoPECache,
+        mask: torch.Tensor,
+        max_seq_length: int,
+        input_pos: Optional[torch.Tensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> Tuple[torch.Tensor, Optional[KVCache]]:
+        h, new_kv_cache = self.attn(self.norm_1(x), rope, mask, max_seq_length, input_pos, kv_cache)
         if self.parallel_residual:
-            x = x + self.attn(self.norm_1(x)) + self.mlp(self.norm_2(x))
+            x = x + h + self.mlp(self.norm_2(x))
         else:
-            x = x + self.attn(self.norm_1(x))
+            x = x + h
             x = x + self.mlp(self.norm_2(x))
-        return x
+        return x, new_kv_cache
 
 
 class Parrot(BaseModel):
@@ -136,6 +155,10 @@ class Parrot(BaseModel):
                 ln_f=nn.LayerNorm(config.n_embd),
             )
         )
+
+        self.rope_cache: Optional[RoPECache] = None
+        self.mask_cache: Optional[torch.Tensor] = None
+        self.kv_caches: List[KVCache] = []
 
     @classmethod
     def from_name(cls, name: str, **kwargs: Any) -> Self:
