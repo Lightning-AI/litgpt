a92844a41e345f9b7bcd395a0f433bf4fb268269
Luca Antiga <luca@lightning.ai>
2023-05-16 09:23:54 +0200

    Port packed_dataset hotfix from lit-llama

--- lit_parrot/packed_dataset.py
+++ lit_parrot/packed_dataset.py
@@ -180,6 +180,8 @@ class PackedDatasetIterator:
 
     def _load_n_chunks(self):
         self._close_mmaps()
+        self._mmaps = []
+        self._buffers = []
 
         if self._n_chunks > len(self._filenames[self._file_idx :]):
             if not self._wrap:
@@ -213,6 +215,7 @@ class PackedDatasetIterator:
     def __del__(self):
         self._close_mmaps()
         del self._mmaps
+        del self._buffers
 
     def __iter__(self):
         return self
