ae144538ff3e751619d7b2c9710c56fce7c495c5
gkroiz <gersonkroiz@google.com>
2023-06-07 21:02:26 +0000

    added xla changes to adapter_v2

--- finetune/adapter.py
+++ finetune/adapter.py
@@ -60,8 +60,8 @@ def setup(
         else XLAStrategy(sync_module_states=False) if tpu else DeepSpeedStrategy(config=ds_config)
     )
     # For multi-host TPU training, the device count for Fabric is limited to the count on a single host.
-    devices = "auto" if (tpu and devices > 1) else devices
-    fabric = L.Fabric(devices=devices, strategy=strategy, precision="32-true" if tpu else precision)
+    fabric_devices = "auto" if (tpu and devices > 1) else devices
+    fabric = L.Fabric(devices=fabric_devices, strategy=strategy, precision="32-true" if tpu else precision)
     fabric.launch(main, data_dir, checkpoint_dir, out_dir)
 
 
