690cae41d9d1ca31f8c4f35c58624b919e5474c6
Carlos Mocholí <carlossmocholi@gmail.com>
2023-06-09 06:32:02 +0200

    openwebtext

---
+++ lit_parrot/speed_monitor.py
@@ -0,0 +1,255 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+# Adapted for standalone use
+from collections import deque
+from typing import Deque, Optional
+
+import torch
+from lightning.fabric.loggers import Logger
+
+from lit_parrot import Parrot
+
+GPU_AVAILABLE_FLOPS = {
+    # source: https://resources.nvidia.com/en-us-tensor-core/nvidia-tensor-core-gpu-datasheet
+    # nvidia publishes spec sheet with a 2x sparsity factor
+    "h100-sxm": {
+        "64-true": 67e12,
+        "32-true": 67e12,
+        "16-true": 1.979e15 / 2,
+        "16-mixed": 1.979e15 / 2,
+        "b16-true": 1.979e15 / 2,
+        "b16-mixed": 1.979e15 / 2,
+        "8-true": 3.958e15 / 2,
+        "8-mixed": 3.958e15 / 2,
+    },
+    "h100-pcie": {
+        "64-true": 51e12,
+        "32-true": 51e12,
+        "16-true": 1.513e15 / 2,
+        "16-mixed": 1.513e15 / 2,
+        "b16-true": 1.513e15 / 2,
+        "b16-mixed": 1.513e15 / 2,
+        "8-true": 3.026e15 / 2,
+        "8-mixed": 3.026e15 / 2,
+    },
+    # source: https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/a100/pdf/nvidia-a100-datasheet-us-nvidia-1758950-r4-web.pdf
+    # sxm and pcie have same flop counts
+    "a100": {
+        "64-true": 19.5e12,
+        "32-true": 19.5e12,
+        "16-true": 312e12,
+        "16-mixed": 312e12,
+        "b16-true": 312e12,
+        "b16-mixed": 312e12,
+    },
+    # source: https://images.nvidia.com/content/technologies/volta/pdf/volta-v100-datasheet-update-us-1165301-r5.pdf
+    "v100-sxm": {"64-true": 7.8e12, "32-true": 15.7e12, "16-true": 125e12, "16-mixed": 125e12},
+    "v100-pcie": {"64-true": 7e12, "32-true": 14e12, "16-true": 112e12, "16-mixed": 112e12},
+    "v100s-pcie": {"64-true": 8.2e12, "32-true": 16.4e12, "16-true": 130e12, "16-mixed": 130e12},
+    # source: https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/tesla-t4/t4-tensor-core-datasheet-951643.pdf
+    # sxm and pcie have same flop counts
+    "t4": {"32-true": 8.1e12, "16-true": 65e12, "16-mixed": 65e12, "8-true": 130e12, "int4": 260e12},
+    # https://www.nvidia.com/content/dam/en-zz/Solutions/design-visualization/quadro-product-literature/quadro-rtx-5000-data-sheet-us-nvidia-704120-r4-web.pdf
+    "quadro rtx 5000": {"32-true": 11.2e12, "16-true": 89.2e12, "16-mixed": 89.2e12},
+}
+
+
+def get_gpu_flops_available(precision: str):
+    # Return 0 if no CUDA device (e.g., when running with CPU only)
+    if not torch.cuda.is_available():
+        return 0
+
+    # torch.cuda.get_device_name() ex output: 'NVIDIA A100-SXM4-40GB'
+    device_name = torch.cuda.get_device_name().lower()
+    if "h100" in device_name and "hbm3" in device_name:
+        device_name = "h100-sxm"
+    elif "h100" in device_name and ("pcie" in device_name or "hbm2e" in device_name):
+        device_name = "h100-pcie"
+    elif "a100" in device_name:
+        device_name = "a100"
+    elif "v100-sxm" in device_name:
+        device_name = "v100-sxm"
+    elif "v100-pcie" in device_name:
+        device_name = "v100-pcie"
+    elif "t4" in device_name:
+        device_name = "t4"
+    elif "quadro rtx 5000" in device_name:
+        device_name = "quadro rtx 5000"
+    else:
+        device_name = None
+
+    gpu_flops_available = None
+    if device_name is not None:
+        try:
+            gpu_flops_available = int(GPU_AVAILABLE_FLOPS[device_name][precision])
+        except KeyError:
+            raise KeyError(
+                f"gpu_flop count not found for {device_name} with precision: {precision}; "
+                "MFU cannot be calculated and reported. gpu_flops_available can be manually "
+                "overridden by setting gpu_flops_available in SpeedMonitor."
+            )
+
+    return gpu_flops_available
+
+
+class SpeedMonitor:
+    """Logs the training throughput and utilization.
+
+    +-------------------------------------+-----------------------------------------------------------+
+    | Key                                 | Logged data                                               |
+    +=====================================+===========================================================+
+    |                                     | Rolling average (over `window_size` most recent           |
+    | `throughput/batches_per_sec`        | batches) of the number of batches processed per second    |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | Rolling average (over `window_size` most recent           |
+    | `throughput/samples_per_sec`        | batches) of the number of samples processed per second    |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | Rolling average (over `window_size` most recent           |
+    | `throughput/tokens_per_sec`         | batches) of the number of tokens processed per second.    |
+    |                                     | This may include padding depending on dataset             |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | Estimates flops by `flops_per_batch * batches_per_sec`    |
+    | `throughput/flops_per_sec`          |                                                           |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    | `throughput/device/batches_per_sec` | `throughput/batches_per_sec` divided by world size        |
+    +-------------------------------------+-----------------------------------------------------------+
+    | `throughput/device/samples_per_sec` | `throughput/samples_per_sec` divided by world size        |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | `throughput/tokens_per_sec` divided by world size. This   |
+    | `throughput/device/tokens_per_sec`  | may include pad tokens depending on dataset               |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | `throughput/flops_per_sec` divided by world size. Only    |
+    | `throughput/device/flops_per_sec`   | logged when model has attribute `flops_per_batch`         |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    |                                     | `throughput/device/flops_per_sec` divided by world size.  |
+    | `throughput/device/mfu`             |                                                           |
+    |                                     |                                                           |
+    +-------------------------------------+-----------------------------------------------------------+
+    | `time/train`                        | Total elapsed training time                               |
+    +-------------------------------------+-----------------------------------------------------------+
+    | `time/val`                          | Total elapsed validation time                             |
+    +-------------------------------------+-----------------------------------------------------------+
+    | `time/total`                        | Total elapsed time (time/train + time/val)                |
+    +-------------------------------------+-----------------------------------------------------------+
+
+    Args:
+        window_size (int, optional): Number of batches to use for a rolling average of throughput.
+            Defaults to 100.
+        time_unit (str, optional): Time unit to use for `time` logging. Can be one of
+            'seconds', 'minutes', 'hours', or 'days'. Defaults to 'hours'.
+    """
+
+    def __init__(self, logger: Logger, precision: str, window_size: int = 100, time_unit: str = "hours"):
+        self.logger = logger
+        self.gpu_flops_available = get_gpu_flops_available(precision)
+
+        # Track the batch num samples and wct to compute throughput over a window of batches
+        self.history_samples: Deque[int] = deque(maxlen=window_size + 1)
+        self.history_wct: Deque[float] = deque(maxlen=window_size + 1)
+        self.history_flops: Deque[float] = deque(maxlen=window_size + 1)
+
+        self.divider = 1
+        if time_unit == "seconds":
+            self.divider = 1
+        elif time_unit == "minutes":
+            self.divider = 60
+        elif time_unit == "hours":
+            self.divider = 60 * 60
+        elif time_unit == "days":
+            self.divider = 60 * 60 * 24
+        else:
+            raise ValueError(
+                f'Invalid time_unit: {time_unit}. Must be one of "seconds", "minutes", "hours", or "days".'
+            )
+
+        # Keep track of time spent evaluating
+        self.total_eval_wct = 0.0
+        self.step = -1
+
+    def batch_end(
+        self,
+        samples: int,
+        train_elapsed: float,  # seconds
+        world_size: int,
+        flops_per_batch: Optional[float] = None,
+        max_seq_length: Optional[int] = None,
+    ):
+        # Add the new element
+        self.history_samples.append(samples)
+        self.history_wct.append(train_elapsed)
+
+        self.step += 1
+        step = self.step
+
+        # Log the throughput
+        if len(self.history_wct) == self.history_wct.maxlen:
+            elapsed_batches = len(self.history_samples) - 1
+            elapsed_samples = int(self.history_samples[-1]) - int(self.history_samples[0])
+            elapsed_wct = self.history_wct[-1] - self.history_wct[0]
+            batches_per_sec = elapsed_batches / elapsed_wct
+            samples_per_sec = elapsed_samples / elapsed_wct
+            dev_batches_per_sec = batches_per_sec / world_size
+            dev_samples_per_sec = samples_per_sec / world_size
+            self.logger.log_metrics({"throughput/batches_per_sec": batches_per_sec}, step)
+            self.logger.log_metrics({"throughput/samples_per_sec": samples_per_sec}, step)
+            self.logger.log_metrics({"throughput/device/batches_per_sec": dev_batches_per_sec}, step)
+            self.logger.log_metrics({"throughput/device/samples_per_sec": dev_samples_per_sec}, step)
+
+            # Assumes no padding.
+            if max_seq_length is not None:
+                # Only applicable to seq data / models
+                self.logger.log_metrics({"throughput/tokens_per_sec": samples_per_sec * max_seq_length}, step)
+                self.logger.log_metrics(
+                    {"throughput/device/tokens_per_sec": dev_samples_per_sec * max_seq_length}, step
+                )
+
+        if flops_per_batch is not None:
+            # sum of flops per batch across ranks
+            self.history_flops.append(flops_per_batch * world_size)
+
+        # Log the flops throughput
+        if len(self.history_flops) == self.history_flops.maxlen:
+            elapsed_flops = sum(self.history_flops) - self.history_flops[0]
+            elapsed_wct = self.history_wct[-1] - self.history_wct[0]
+            flops_per_sec = elapsed_flops / elapsed_wct
+            device_flops_per_sec = flops_per_sec / world_size
+            self.logger.log_metrics({"throughput/flops_per_sec": flops_per_sec}, step)
+            self.logger.log_metrics({"throughput/device/flops_per_sec": device_flops_per_sec}, step)
+            if self.gpu_flops_available:
+                mfu = device_flops_per_sec / self.gpu_flops_available
+                self.logger.log_metrics({"throughput/device/mfu": mfu}, step)
+
+        # Log the time
+        self.logger.log_metrics(
+            {
+                "time/train": train_elapsed / self.divider,
+                "time/val": self.total_eval_wct / self.divider,
+                "time/total": (train_elapsed + self.total_eval_wct) / self.divider,
+            },
+            step,
+        )
+
+    def eval_end(self, eval_elapsed: float):
+        self.total_eval_wct += eval_elapsed  # seconds
+
+
+def total_flops(model: Parrot):
+    n_params = sum(p.numel() for p in model.parameters())
+    # credit: https://github.com/mosaicml/llm-foundry/blob/main/scripts/train/benchmarking/collect_results.py#L144-L156
+    # mfu is approximated using thoughtput and param count
+    # the number of paramters is approximately the number of multiply-accumulates (MAC) in the network
+    # each MAC has 2 FLOPs - we multiply by 2 ie 2 * n_param
+    # there are 3 passes of a NN (fwd, bwd, delta) - we multiply by 3 ie 2 * 3 * n_param
+    # this gets us FLOPs / token
+    flops_per_token = 2 * n_params
+    flops_per_seq = flops_per_token * model.config.block_size
+    # there are 2 FLOPS per mac; there is A=Q*K^T and out=A*V ops (ie mult by 2)
+    attn_flops_per_seq = model.config.n_layer * 2 * 2 * (model.config.n_embd * (model.config.block_size**2))
+    # there are 2 ops in bwd pass and 1 in fwd pass so we mult by 3
+    total_flops = 3 * (flops_per_seq + attn_flops_per_seq)
+    return total_flops
---
+++ pretrain/openwebtext.py
@@ -0,0 +1,256 @@
+import json
+import math
+import sys
+import time
+from pathlib import Path
+from types import MethodType
+from typing import Tuple
+
+import lightning as L
+import numpy as np
+import torch
+
+# support running without installing as a package
+wd = Path(__file__).parent.parent.resolve()
+sys.path.append(str(wd))
+
+from lit_parrot import Config, Parrot, Tokenizer
+from lit_parrot.utils import check_valid_checkpoint_dir
+from generate.base import generate
+from lit_parrot.speed_monitor import SpeedMonitor, total_flops
+from lightning.fabric.loggers import CSVLogger
+
+
+devices = 1
+precision = "16-mixed"
+
+out_dir = Path("out") / "openwebtext"
+data_dir = Path("data") / "openwebtext"
+name = "train-openwebtext"
+eval_interval = 200
+save_interval = 400
+eval_iters = 100
+log_interval = 1
+
+# Hyperparameters
+learning_rate = 6e-4
+batch_size = 64 / devices
+micro_batch_size = 4  # FIXME
+gradient_accumulation_steps = batch_size // micro_batch_size
+assert gradient_accumulation_steps > 0
+epoch_size = 50000  # train dataset size
+# num_epochs = 5
+max_iters = 600000  # num_epochs * (epoch_size // micro_batch_size) // devices
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0
+decay_lr = True
+warmup_iters = 2000
+lr_decay_iters = max_iters
+min_lr = 6e-5
+seed = 1338
+
+hparams = {k: v for k, v in locals().items() if isinstance(v, (int, float, str)) and not k.startswith("_")}
+print(hparams)
+
+logger = CSVLogger(out_dir)
+
+
+def merge_by(dicts, key):
+    from collections import defaultdict
+
+    out = defaultdict(dict)
+    for d in dicts:
+        if key in d:
+            out[d[key]].update(d)
+    return [v for _, v in sorted(out.items())]
+
+
+def save(self) -> None:
+    """Overriden to merge CSV by the step number."""
+    import csv
+
+    if not self.metrics:
+        return
+    metrics = merge_by(self.metrics, "step")
+    keys = sorted({k for m in metrics for k in m})
+    with self._fs.open(self.metrics_file_path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(metrics)
+
+
+logger.experiment.save = MethodType(save, logger.experiment)
+
+speed_monitor = SpeedMonitor(logger, precision, window_size=50, time_unit="seconds")
+
+
+def main(checkpoint_dir: Path = Path(f"checkpoints/stabilityai/stablelm-base-alpha-3b")) -> None:
+    fabric = L.Fabric(devices=devices, precision=precision)
+    fabric.launch()
+
+    check_valid_checkpoint_dir(checkpoint_dir)
+
+    with open(checkpoint_dir / "lit_config.json") as fp:
+        config = Config(**json.load(fp))
+
+    fabric.seed_everything(seed + fabric.global_rank)
+
+    if fabric.global_rank == 0:
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+    train_data, val_data = load_datasets(data_dir)
+
+    fabric.print(f"Loading model with {config.__dict__}", file=sys.stderr)
+    t0 = time.time()
+    with fabric.init_module():
+        model = Parrot(config)
+    fabric.print(f"Time to instantiate model: {time.time() - t0:.02f} seconds.", file=sys.stderr)
+
+    num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    num_total_params = sum(p.numel() for p in model.parameters())
+    fabric.print("Total parameters (M)", num_total_params, ", trainable parameters (M)", num_trainable_params)
+
+    tokenizer = Tokenizer(checkpoint_dir / "tokenizer.json", checkpoint_dir / "tokenizer_config.json")
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(beta1, beta2))
+    model, optimizer = fabric.setup(model, optimizer)
+
+    train(fabric, model, tokenizer, optimizer, train_data, val_data)
+
+
+def train(
+    fabric: L.Fabric,
+    model: Parrot,
+    tokenizer: Tokenizer,
+    optimizer: torch.optim.Optimizer,
+    train_data: np.ndarray,
+    val_data: np.ndarray,
+) -> None:
+    validate(fabric, model, tokenizer, val_data)  # sanity check
+
+    flops = total_flops(model)
+    print("Model TFLOPs", flops / 1e12)
+    step_count = 0
+    total_t0 = time.time()
+
+    for iter_num in range(max_iters):
+        # determine and set the learning rate for this iteration
+        lr = get_lr(iter_num) if decay_lr else learning_rate
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
+
+        did_step = False
+        iter_t0 = time.time()
+
+        input_ids, targets = get_batch(fabric, train_data, model.config.block_size)
+        with fabric.no_backward_sync(model, enabled=((iter_num + 1) % gradient_accumulation_steps != 0)):
+            logits = model(input_ids)
+            loss = torch.nn.functional.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+            fabric.backward(loss / gradient_accumulation_steps)
+
+        if (iter_num + 1) % gradient_accumulation_steps == 0:
+            fabric.clip_gradients(model, optimizer, max_norm=grad_clip)
+            optimizer.step()
+            optimizer.zero_grad()
+            step_count += 1
+            did_step = True
+
+        t1 = time.time()
+        speed_monitor.batch_end(
+            (iter_num + 1) * micro_batch_size,
+            t1 - total_t0,
+            fabric.world_size,
+            flops_per_batch=flops * micro_batch_size,
+            max_seq_length=model.config.block_size,
+        )
+        if iter_num % log_interval == 0:
+            fabric.print(
+                f"iter {iter_num} step {step_count}: loss {loss.item():.4f}, train time:"
+                f" {(t1 - iter_t0) * 1000:.2f}ms{' (optimizer.step)' if did_step else ''}"
+            )
+
+        if did_step and step_count % eval_interval == 0:
+            t0 = time.time()
+            val_loss = validate(fabric, model, tokenizer, val_data)
+            t1 = time.time() - t0
+            speed_monitor.eval_end(t1)
+            fabric.print(f"step {iter_num}: val loss {val_loss:.4f}, val time: {t1 * 1000:.2f}ms")
+            fabric.barrier()
+        if did_step and step_count % save_interval == 0:
+            checkpoint_path = out_dir / f"{name}.pth"
+            fabric.print(f"Saving checkpoint to {str(checkpoint_path)!r}")
+            fabric.save(checkpoint_path, {"model": model})
+
+
+@torch.no_grad()
+def validate(
+    fabric: L.Fabric, model: torch.nn.Module, tokenizer: Tokenizer, val_data: np.ndarray, num_samples: int = 3
+) -> torch.Tensor:
+    fabric.print("Validating ...")
+    model.eval()
+
+    losses = torch.zeros(eval_iters, device=fabric.device)
+    for k in range(eval_iters):
+        input_ids, targets = get_batch(fabric, val_data, model.config.block_size)
+        logits = model(input_ids)
+        loss = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        losses[k] = loss.item()
+    out = losses.mean()
+
+    input_ids, _ = get_batch(fabric, val_data, model.config.block_size)
+    input_ids = input_ids[:, :num_samples]
+    for ids in input_ids:
+        prompt = tokenizer.decode(ids)
+        y = generate(
+            model,
+            ids,
+            max_returned_tokens=ids.size(0) + 100,
+            max_seq_length=model.config.block_size,
+            temperature=0.8,
+            top_k=50,
+        )
+        fabric.print(f"Prompt: {prompt!r}: {tokenizer.decode(y)!r}")
+
+    model.train()
+    return out
+
+
+def get_batch(fabric: L.Fabric, data: np.ndarray, block_size: int) -> Tuple[torch.Tensor, torch.Tensor]:
+    ix = torch.randint(len(data) - block_size, (micro_batch_size,))
+    x = torch.stack([torch.from_numpy((data[i : i + block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i + 1 : i + 1 + block_size]).astype(np.int64)) for i in ix])
+    x, y = fabric.to_device((x.pin_memory(), y.pin_memory()))
+    return x, y
+
+
+def load_datasets(data_dir: Path) -> Tuple[np.ndarray, np.ndarray]:
+    train_data = np.memmap(str(data_dir / "train.bin"), dtype=np.uint16, mode="r")
+    val_data = np.memmap(str(data_dir / "val.bin"), dtype=np.uint16, mode="r")
+    return train_data, val_data
+
+
+# learning rate decay scheduler (cosine with warmup)
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_iters:
+        return learning_rate * it / warmup_iters
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > lr_decay_iters:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff ranges 0..1
+    return min_lr + coeff * (learning_rate - min_lr)
+
+
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision("high")
+
+    from jsonargparse import CLI
+
+    CLI(main)
