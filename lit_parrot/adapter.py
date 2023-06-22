c31dc7ed3744c1ca10113f690f31355db5125138
Carlos Mocholí <carlossmocholi@gmail.com>
2023-06-22 15:06:43 +0200

    Port missing model change to adapter

--- lit_parrot/adapter.py
+++ lit_parrot/adapter.py
@@ -67,7 +67,8 @@ class Parrot(BaseModel):
         block_size = self.config.block_size
         if max_seq_length is None:
             max_seq_length = block_size
-        assert T <= max_seq_length, f"Cannot forward sequence of length {T}, max seq length is only {max_seq_length}"
+        if use_kv_cache:  # not relevant otherwise
+            assert T <= max_seq_length, f"Cannot forward sequence of length {T}, max seq length is only {max_seq_length}"
         assert max_seq_length <= block_size, f"Cannot attend to {max_seq_length}, block size is only {block_size}"
         assert T <= block_size, f"Cannot forward sequence of length {T}, block size is only {block_size}"
 
