67b05c6a83a33b1e7488c4662b25fe42d6ceda0e
Carlos Mocholí <carlossmocholi@gmail.com>
2023-06-23 02:29:05 +0200

    Comment

--- finetune/adapter_v2.py
+++ finetune/adapter_v2.py
@@ -100,7 +100,7 @@ def main(fabric: L.Fabric, data_dir: Path, checkpoint_dir: Path, out_dir: Path):
     fabric.print(f"Loading model {str(checkpoint_path)!r} with {config.__dict__}")
     with fabric.init_module(empty_init=False):
         model = GPT(config)
-        model.apply(model._init_weights)
+        model.apply(model._init_weights)  # for the adapter weights
     with lazy_load(checkpoint_path) as checkpoint:
         # strict=False because missing keys due to adapter weights not contained in state dict
         model.load_state_dict(checkpoint, strict=False)
