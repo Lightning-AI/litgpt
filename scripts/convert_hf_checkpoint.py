beaed8a7f0e92b17ff1d8cc370baefd83f46da2a
Carlos Mocholí <carlossmocholi@gmail.com>
2023-06-29 16:41:08 +0200

    Consistent LLaMA naming (#214)
    Co-authored-by: Luca Antiga <luca@lightning.ai>

--- scripts/convert_hf_checkpoint.py
+++ scripts/convert_hf_checkpoint.py
@@ -206,7 +206,7 @@ def convert_hf_checkpoint(
 
     if "falcon" in model_name:
         copy_fn = partial(copy_weights_falcon, "40b" if config.n_embd == 8192 else "7b")
-    elif "open_llama" in model_name:
+    elif config._mlp_class == "LLaMAMLP":
         # holder to reconstitute the split q, k, v
         qkv_weights = {}
         copy_fn = partial(copy_weights_open_llama, config, qkv_weights)
