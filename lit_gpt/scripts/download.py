0a683a37eb99fa72c3bb61daa7f1112490df48f3
louisoutin <louis.outin@gmail.com>
2023-07-10 21:21:12 +0100

    return checkpoint dir string

--- lit_gpt/scripts/download.py
+++ lit_gpt/scripts/download.py
@@ -7,7 +7,7 @@ wd = Path(__file__).parent.parent.resolve()
 sys.path.append(str(wd))
 
 
-def download_from_hub(repo_id: Optional[str] = None, directory: Optional[str] = None) -> None:
+def download_from_hub(repo_id: Optional[str] = None, directory: Optional[str] = None) -> str:
     if directory is None:
         directory = "checkpoints/"
 
@@ -17,17 +17,20 @@ def download_from_hub(repo_id: Optional[str] = None, directory: Optional[str] = 
         options = [f"{config['org']}/{config['name']}" for config in configs]
         print("Please specify --repo_id <repo_id>. Available values:")
         print("\n".join(options))
-        return
+        return ""
 
     from huggingface_hub import snapshot_download
 
+    checkpoint_dir: str = str(Path(directory) / Path(repo_id))
+
     snapshot_download(
         repo_id,
-        local_dir=str(Path(directory) / Path(repo_id)),
+        local_dir=checkpoint_dir,
         local_dir_use_symlinks=False,
         resume_download=True,
         allow_patterns=["*.bin*", "tokenizer*"],
     )
+    return checkpoint_dir
 
 
 if __name__ == "__main__":
