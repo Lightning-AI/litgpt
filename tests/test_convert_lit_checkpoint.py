1cc5d5ba0a33d41535b33024cca5eb8613ae0cd2
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-07-17 17:27:58 -0400

    add hasty test

---
+++ tests/test_convert_lit_checkpoint.py
@@ -0,0 +1,21 @@
+import torch
+import json
+
+if __name__ == "__main__":
+    lit_hf_ckpt = "checkpoints/tiiuae/falcon-7b/lit_hf_model.bin"
+    lit_hf_model = torch.load(lit_hf_ckpt)
+
+    lit_hf_weights_keys = [k for k in lit_hf_model.keys()]
+
+    hf_keys_path = "checkpoints/tiiuae/falcon-7b/pytorch_model.bin.index.json"
+
+    with open(hf_keys_path, "r") as json_file:
+        hf_md = json.load(json_file)
+
+    hf_weight_map = hf_md["weight_map"]
+
+    assert all(
+        litkey in list(hf_weight_map.keys()) for litkey in list(lit_hf_weights_keys)
+    ), "does not match"
+
+    print("passed")
