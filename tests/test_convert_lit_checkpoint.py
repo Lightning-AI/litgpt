a54d4bf383e87cf2986421d3f484647c58053f5a
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-07-18 13:17:12 -0400

    update test_falcon_conversion

--- tests/test_convert_lit_checkpoint.py
+++ tests/test_convert_lit_checkpoint.py
@@ -1,21 +1,22 @@
 import torch
 import json
+import os
+import pytest
 
-if __name__ == "__main__":
-    lit_ckpt = "checkpoints/tiiuae/falcon-7b/lit_model_finetuned.bin"
-    lit_model = torch.load(lit_ckpt)
 
+@pytest.mark.parametrize("model_size", ["7b", "40b"])
+def test_falcon_conversion(model_size):
+    chkptpath = f"checkpoints/tiiuae/falcon-{model_size}/lit_model_finetuned.bin"
+
+    if not os.path.isfile(chkptpath):
+        pytest.skip(f"{chkptpath} not found")
+
+    lit_model = torch.load(chkptpath)
     lit_weight_keys = [k for k in lit_model.keys()]
 
     hf_keys_path = "checkpoints/tiiuae/falcon-7b/pytorch_model.bin.index.json"
-
     with open(hf_keys_path, "r") as json_file:
         hf_md = json.load(json_file)
+    hf_weight_keys = list(hf_md["weight_map"])
 
-    hf_weight_map = hf_md["weight_map"]
-
-    assert all(
-        litkey in list(hf_weight_map.keys()) for litkey in lit_weight_keys
-    ), "does not match"
-
-    print("passed")
+    assert all(litkey in hf_weight_keys for litkey in lit_weight_keys)
