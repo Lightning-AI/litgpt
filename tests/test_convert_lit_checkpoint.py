ee894af7f93c27691fbcbfbe5da3b1349355fe5a
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-07-21 11:08:02 -0400

    clean tests

--- tests/test_convert_lit_checkpoint.py
+++ tests/test_convert_lit_checkpoint.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from urllib.request import urlretrieve
 
-import pytest
 import torch
 
 wd = Path(__file__).parent.parent.absolute()
@@ -15,64 +14,27 @@ def test_against_original_falcon_40b():
         urlretrieve(url=url, filename=file_path)
 
     from tests.original_falcon_40b import RWConfig, RWForCausalLM
-    from lit_gpt import Config, GPT
     from scripts.convert_lit_checkpoint import copy_weights_falcon as copy_to_theirs
     from scripts.convert_hf_checkpoint import copy_weights_falcon as copy_to_ours
 
-    # the unchanged model config
-    our_config = Config.from_name("falcon-40b", n_layer=2, n_head=8, n_query_groups=4, n_embd=32)
     theirs_config = RWConfig(
         hidden_size=32, n_head=8, n_head_kv=4, n_layer=2, parallel_attn=True, vocab_size=65024, bias=False
     )
 
-    # need to begin with weight initialized from a ground truth source
-    ground_truth_their_model = RWForCausalLM(theirs_config)
-    ground_truth_their_state_dict = ground_truth_their_model.state_dict()
+    their_model = RWForCausalLM(theirs_config)
+    their_state_dict = their_model.state_dict()
 
     our_state_dict = {}
-    copy_weights_falcon_hf("40b", our_state_dict, ground_truth_their_state_dict)
-    our_model = GPT(our_config)
-    our_model.load_state_dict(our_state_dict)
+    copy_to_ours("40b", our_state_dict, their_state_dict)
 
-    their_state_dict = {}
-    copy_weights_falcon("40b", their_state_dict, our_state_dict)
-    their_model = RWForCausalLM(theirs_config)
-    # loading their_state_dict here is an assertion
-    # that all weights the model expects are available
-    # else a RuntimeError would occur while loading
-    their_model.load_state_dict(their_state_dict)
-    # check that all tensors that went into the conversion
-    # are saved in the correct order
-    assert all(
-        [
-            torch.equal(their_state_dict[tsdk], our_state_dict[osdk])
-            for tsdk, osdk in zip(their_state_dict.keys(), our_state_dict.keys())
-        ]
-    )
+    converted_state_dict = {}
+    copy_to_theirs("40b", converted_state_dict, our_state_dict)
+
+    converted_model = RWForCausalLM(theirs_config)
+    converted_model.load_state_dict(converted_state_dict)
 
     # test end to end
     x = torch.tensor([[9856, 23, 491, 1536, 304]], dtype=torch.int32)
-    our_y = our_model(x)
+    converted_y = converted_model(x)["logits"]
     their_y = their_model(x)["logits"]
-    ground_truth_y = ground_truth_their_model(x)["logits"]
-    torch.testing.assert_close(our_y, ground_truth_y)
-    torch.testing.assert_close(our_y, their_y)
-
-
-@torch.inference_mode()
-@pytest.mark.skip("not implemented")
-def test_against_original_open_llama_3b():
-    pass
-
-
-@torch.inference_mode()
-@pytest.mark.skip("not implemented")
-@pytest.mark.parametrize("size", ("7b", "70b"))
-def test_against_hf_llama2(size):
-    pass
-
-
-@torch.inference_mode()
-@pytest.mark.skip("not implemented")
-def test_against_gpt_neox(size):
-    pass
+    torch.testing.assert_close(converted_y, their_y)
