fc4c14d89e62799eeae162548b6aec24f1ab901a
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-07-24 22:25:07 -0400

    Convert Lit-GPT Falcon Format to Original Falcon Format (#273)
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

---
+++ tests/test_convert_lit_checkpoint.py
@@ -0,0 +1,63 @@
+from unittest import mock
+from pathlib import Path
+from urllib.request import urlretrieve
+
+import pytest
+import torch
+
+wd = Path(__file__).parent.parent.absolute()
+
+
+def test_convert_lit_checkpoint(tmp_path):
+    from scripts.convert_lit_checkpoint import convert_lit_checkpoint
+
+    ckpt_name = "lit_model_finetuned"
+
+    with pytest.raises(RuntimeError, match="open file failed because of errno 2 on fopen"):
+        convert_lit_checkpoint(checkpoint_name=ckpt_name, checkpoint_dir=tmp_path, model_name="falcon-7b")
+
+    ckpt_path = tmp_path / "lit_model_finetuned"
+    ckpt_path.touch()
+    with mock.patch("scripts.convert_lit_checkpoint.lazy_load") as load:
+        convert_lit_checkpoint(checkpoint_name=ckpt_name, checkpoint_dir=tmp_path, model_name="falcon-7b")
+    load.assert_called_with(ckpt_path)
+
+    assert {p.name for p in tmp_path.glob("*")} == {ckpt_name, "lit_model_finetuned.bin"}
+
+
+@torch.inference_mode()
+def test_against_original_falcon_40b():
+    file_path = wd / "tests" / "original_falcon_40b.py"
+    url = "https://gist.githubusercontent.com/carmocca/feed39b1bc65a29f73c1cecc58a01167/raw/a9a65f2b93716b3c09ec9f354d535ae5953de08f/original_falcon_40b.py"
+    if not file_path.is_file():
+        urlretrieve(url=url, filename=file_path)
+
+    from tests.original_falcon_40b import RWConfig, RWForCausalLM
+    from lit_gpt import Config, GPT
+    from scripts.convert_lit_checkpoint import copy_weights_falcon as copy_to_theirs
+
+    ours_config = Config.from_name("falcon-40b", n_layer=2, n_head=8, n_query_groups=4, n_embd=32)
+    theirs_config = RWConfig(
+        hidden_size=32,
+        n_head=8,
+        n_head_kv=4,
+        n_layer=2,
+        parallel_attn=True,
+        vocab_size=65024,
+        bias=False,
+    )
+
+    ours_model = GPT(ours_config)
+    ours_state_dict = ours_model.state_dict()
+    theirs_state_dict = {}
+    copy_to_theirs("40b", theirs_state_dict, ours_state_dict)
+
+    theirs_model = RWForCausalLM(theirs_config)
+    # assign must be set to True for torch.testing.assert_close to pass
+    theirs_model.load_state_dict(theirs_state_dict, strict=False, assign=True)
+
+    # test end to end
+    x = torch.tensor([[9856, 23, 491, 1536, 304]], dtype=torch.int32)
+    ours_y = ours_model(x)
+    theirs_y = theirs_model(x)["logits"]
+    torch.testing.assert_close(ours_y, theirs_y)
---
+++ tutorials/convert_lit_models.md
@@ -0,0 +1,38 @@
+## Converting a Lit-GPT Formatted Model to its Original Format
+
+Several models retrieved from external sources must be reformatted with naming conventions for Lit-GPT weights before finetuning. We've provided a helpful script to convert models finetuned with Lit-GPT back to their original format.
+
+The way the script is used depends on the finetuning method. The commands for converting a model back to its original format after using one of the several finetuning methods supported by Lit-GPT are shown below.
+
+> [!NOTE]\
+> each example shown below uses Falcon-7B please be sure to update --checkpoint_dir accordingly
+
+### Full Finetuning
+
+After full finetuning, your checkpoint directory will contain a file named `lit_model_finetuned.pth`and converting the finetuned model back to its original weights naming convention can be done by setting the `--checkpoint_name` with:
+
+```sh
+python scripts/convert_lit_checkpoint.py \
+    --checkpoint_dir=checkpoints/tiiuae/falcon-7b \
+    --checkpoint_name=lit_model_finetuned
+```
+
+### Adapter and Adapter V2 Finetuning
+
+After finetuning with either Adapter technique, your checkpoint directory will contain a file named `lit_model_adapter_finetuned.pth` and converting the finetuned model back to its original weights naming convention can be done by setting the `--checkpoint_name` with:
+
+```sh
+python scripts/convert_lit_checkpoint.py \
+    --checkpoint_dir=checkpoints/tiiuae/falcon-7b \
+    --checkpoint_name=lit_model_adapter_finetuned
+```
+
+### LoRA Finetuning
+
+After finetuning with LoRA, your checkpoint directory will contain a file named `lit_model_lora_finetuned.pth` and converting the finetuned model back to its original weights naming convention can be done by setting the `--checkpoint_name` with:
+
+```sh
+python scripts/convert_lit_checkpoint.py \
+    --checkpoint_dir=checkpoints/tiiuae/falcon-7b \
+    --checkpoint_name=lit_model_lora_finetuned
+```
