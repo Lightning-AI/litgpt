7b5b19d08cdb4240094be89f87275985b85c45ce
Andrei-Aksionov <58434077+Andrei-Aksionov@users.noreply.github.com>
2023-08-14 15:37:48 +0300

    Make qkv_shapes definition more readable
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- lit_gpt/lora.py
+++ lit_gpt/lora.py
@@ -211,12 +211,8 @@ class LoRAQKVLinear(LoRALinear):
             enable_q, enable_k, enable_v = enable_lora
             self.kv_embd_size = self.linear.in_features // (n_head // n_query_groups)
             # qkv_shapes will be used to split a tensor with weights correctly
-            self.qkv_shapes = tuple(
-                filter(
-                    None,
-                    (self.linear.in_features * enable_q, self.kv_embd_size * enable_k, self.kv_embd_size * enable_v),
-                )
-            )
+            qkv_shapes = (self.linear.in_features * enable_q, self.kv_embd_size * enable_k, self.kv_embd_size * enable_v)
+            self.qkv_shapes = [s for s in qkv_shapes if s is not None]
             self.lora_B = nn.Parameter(self.linear.weight.new_zeros(sum(self.qkv_shapes), r))  # (256, 2))
             # Notes about shapes above
             # - self.lora_A has shape (4, 128): 4 because rank is 2 and LoRA is applied only to two matrices;
