a474126e5d8fad478209be2dd34f717ba2f34afb
Carlos Mocholí <carlossmocholi@gmail.com>
2023-09-12 00:42:11 +0200

    Fixes and test

--- scripts/prepare_csv.py
+++ scripts/prepare_csv.py
@@ -1,16 +1,11 @@
 import json
 import logging
 import sys
-import warnings
 from pathlib import Path
-
-import pandas as pd
-from tqdm import tqdm
-
-warnings.filterwarnings("ignore")
 
 import torch
 from torch.utils.data import random_split
+from tqdm import tqdm
 
 # support running without installing as a package
 wd = Path(__file__).parent.parent.resolve()
@@ -19,55 +14,49 @@ sys.path.append(str(wd))
 
 from lit_gpt.tokenizer import Tokenizer
 
-SEED = 42
-MASK_INPUTS = False
-IGNORE_INDEX = -1
-TEST_SPLIT_FRACTION = 0.1
-CHECKPOINT_DIR = Path("checkpoints/EleutherAI/pythia-70m")
-DESTINATION_DIR = Path("data/mydata")
 COLUMNS = ("instruction", "input", "output")
 
+
 def prepare(
-    csv_path: Path = None,
-    destination_path: Path = DESTINATION_DIR,
-    checkpoint_dir: Path = CHECKPOINT_DIR,
-    test_split_fraction: float = TEST_SPLIT_FRACTION,
-    seed: int = SEED,
-    mask_inputs: bool = MASK_INPUTS,
-    ignore_index: int = IGNORE_INDEX,
+    csv_path: Path,
+    destination_path: Path = Path("data/csv"),
+    checkpoint_dir: Path = Path("checkpoints/stabilityai/stablelm-base-alpha-3b"),
+    test_split_fraction: float = 0.1,
+    seed: int = 42,
+    mask_inputs: bool = False,
+    ignore_index: int = -1,
 ) -> None:
-    """Prepare dataset for instruction tuning from csv
+    """Prepare a CSV dataset for instruction tuning.
 
     The output is a training and test dataset saved as `train.pt` and `test.pt`,
     which stores the preprocessed and tokenized prompts and labels.
     """
-    # TODO: Provide args inside docstrings
-
     with open(checkpoint_dir / "lit_config.json", "r") as file:
         config = json.load(file)
         max_seq_length = config["block_size"]
 
     destination_path.mkdir(parents=True, exist_ok=True)
     logger.info("Loading data file ...")
-    df = pd.read_csv(csv_path).fillna("")
-    assert (df.columns.values == COLUMNS).all(), f"CSV columns must be {COLUMNS}"
-    df_json = json.loads(df.to_json(orient="records", indent=4))
+    import pandas as pd
 
-    logger.info("Loading tokenizer ...")
+    df = pd.read_csv(csv_path, dtype=str).fillna("")
+    if not (df.columns.values == COLUMNS).all():
+        raise ValueError(f"CSV columns must be {COLUMNS}, found {df.columns.values}")
+    data = json.loads(df.to_json(orient="records", indent=4))
+
+    print("Loading tokenizer...")
     tokenizer = Tokenizer(checkpoint_dir)
 
     # Partition the dataset into train and test
     train_set, test_set = random_split(
-        df_json,
-        [1.0 - test_split_fraction, test_split_fraction],
-        generator=torch.Generator().manual_seed(seed),
+        data, [1.0 - test_split_fraction, test_split_fraction], generator=torch.Generator().manual_seed(seed)
     )
-
     train_set, test_set = list(train_set), list(test_set)
-    logger.info(f"train has {len(train_set):,} samples")
-    logger.info(f"test has {len(test_set):,} samples")
-    logger.info("Processing train split ...")
 
+    print(f"train has {len(train_set):,} samples")
+    print(f"test has {len(test_set):,} samples")
+
+    print("Processing train split ...")
     train_set = [
         prepare_sample(
             example=sample,
@@ -80,7 +69,7 @@ def prepare(
     ]
     torch.save(train_set, destination_path / "train.pt")
 
-    logger.info("Processing test split ...")
+    print("Processing test split ...")
     test_set = [
         prepare_sample(
             example=sample,
@@ -94,30 +83,7 @@ def prepare(
     torch.save(test_set, destination_path / "test.pt")
 
 
-def generate_prompt(example):
-    """Generates a standardized message to prompt the model with an instruction, optional input and a
-    'response' field."""
-
-    if example["input"]:
-        return (
-            "Below is an instruction that describes a task, paired with an input that provides further context. "
-            "Write a response that appropriately completes the request.\n\n"
-            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
-        )
-    return (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        f"### Instruction:\n{example['instruction']}\n\n### Response:"
-    )
-
-
-def prepare_sample(
-    example: dict,
-    tokenizer: Tokenizer,
-    max_length: int,
-    mask_inputs: bool = MASK_INPUTS,
-    ignore_index: int = IGNORE_INDEX,
-):
+def prepare_sample(example: dict, tokenizer: Tokenizer, max_length: int, mask_inputs: bool, ignore_index: int):
     """Processes a single sample.
 
     Each sample in the dataset consists of:
@@ -137,9 +103,7 @@ def prepare_sample(
     full_prompt = generate_prompt(example)
     full_prompt_and_response = full_prompt + example["output"]
     encoded_full_prompt = tokenizer.encode(full_prompt, max_length=max_length)
-    encoded_full_prompt_and_response = tokenizer.encode(
-        full_prompt_and_response, eos=True, max_length=max_length
-    )
+    encoded_full_prompt_and_response = tokenizer.encode(full_prompt_and_response, eos=True, max_length=max_length)
 
     # The labels are the full prompt with response, but with the prompt masked out
     labels = encoded_full_prompt_and_response.clone()
@@ -154,9 +118,24 @@ def prepare_sample(
     }
 
 
-if __name__ == "__main__":
-    from jsonargparse import CLI, ArgumentParser
+def generate_prompt(example):
+    """Generates a standardized message to prompt the model with an instruction, optional input and a
+    'response' field."""
 
-    parser = ArgumentParser()
-    parser.add_function_arguments
-    cli = CLI(prepare)
+    if example["input"]:
+        return (
+            "Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
+        )
+    return (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        f"### Instruction:\n{example['instruction']}\n\n### Response:"
+    )
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(prepare)
