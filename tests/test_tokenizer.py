55faa0aba642ac7625117f137685ec4c3c0984b8
Carlos Mocholí <carlossmocholi@gmail.com>
2023-09-18 18:14:48 +0200

    Replace old test

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -3,7 +3,6 @@ import sys
 from pathlib import Path
 
 import pytest
-import torch
 from transformers import AutoTokenizer
 from transformers.utils import cached_file
 
@@ -14,35 +13,9 @@ sys.path.append(str(wd))
 import lit_gpt.config as config_module
 
 
-def test_tokenizer_against_hf():
-    import lit_gpt
-
-    hf_tokenizer = AutoTokenizer.from_pretrained("StabilityAI/stablelm-base-alpha-3b")
-    # hacky way to access the data loaded by the above
-    folder = Path(hf_tokenizer.init_kwargs["special_tokens_map_file"]).parent
-
-    tokenizer = lit_gpt.Tokenizer(folder)
-
-    assert tokenizer.vocab_size == hf_tokenizer.vocab_size
-    assert tokenizer.eos_id == hf_tokenizer.eos_token_id
-
-    string = "What's your mood today?"
-    actual = tokenizer.encode(string)
-    assert actual.tolist() == hf_tokenizer(string)["input_ids"]
-    assert tokenizer.decode(actual) == hf_tokenizer.decode(actual)
-    assert tokenizer.decode(torch.tensor(0)) == ""
-
-    with pytest.raises(ValueError, match="'foobarbaz' not found"):
-        tokenizer.token_to_id("foobarbaz")
-
-    actual = tokenizer.encode("a b")
-    assert torch.equal(actual, torch.tensor([66, 270])), actual
-    actual = tokenizer.encode("a b", eos=True)
-    assert torch.equal(actual, torch.tensor([66, 270, 0])), actual
-
-
 @pytest.mark.parametrize("config", config_module.configs, ids=[c["name"] for c in config_module.configs])
-def test_against_hf(config):
+@pytest.mark.xfail(raises=AssertionError, reason="To be fixed", strict=False)
+def test_tokenizer_against_hf(config):
     from lit_gpt.tokenizer import Tokenizer
 
     access_token = os.getenv("HF_TOKEN")
@@ -72,17 +45,22 @@ def test_against_hf(config):
             (checkpoint_dir / file).symlink_to(hf_file)
 
     try:
-        theirs = AutoTokenizer.from_pretrained(repo_id, cache_dir=cache_dir / "hf", local_files_only=True, token=access_token)
+        theirs = AutoTokenizer.from_pretrained(
+            repo_id, cache_dir=cache_dir / "hf", local_files_only=True, token=access_token
+        )
     except OSError as e:
         assert "gated repo" in str(e)
         pytest.xfail("Gated repo")
         return
     ours = Tokenizer(checkpoint_dir)
 
+    assert ours.vocab_size == theirs.vocab_size
+    assert ours.bos_id == theirs.bos_token_id
+    assert ours.eos_id == theirs.eos_token_id
+
     prompt = "Hello, readers of this test!"
-    actual = ours.encode(prompt).tolist()
+    actual = ours.encode(prompt)
     expected = theirs.encode(prompt)
-    if ours.backend == "sentencepiece" or config.name == "LLaMA-2-7B-32K":  # TODO: fix this, missing BOS
-        assert actual != expected
-    else:
-        assert actual == expected
+    assert actual.tolist() == expected
+
+    assert ours.decode(actual) == theirs.decode(actual)
