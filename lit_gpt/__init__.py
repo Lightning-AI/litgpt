94189f307fa320e17baf4c3da3c0887ceb1eef19
Carlos Mocholí <carlossmocholi@gmail.com>
2023-10-04 23:21:19 +0200

    Torch 2.1 installation instructions

--- lit_gpt/__init__.py
+++ lit_gpt/__init__.py
@@ -4,10 +4,6 @@ from lit_gpt.tokenizer import Tokenizer
 
 from lightning_utilities.core.imports import RequirementCache
 
-if not bool(RequirementCache("torch>=2.1.0dev")):
-    raise ImportError(
-        "Lit-GPT requires torch==2.1. Please follow the installation instructions in the repository README.md"
-    )
 _LIGHTNING_AVAILABLE = RequirementCache("lightning>=2.1.0.dev0")
 if not bool(_LIGHTNING_AVAILABLE):
     raise ImportError(
