628c8a2cca22383674cb8682bc83862625d45531
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-10-23 22:06:11 +0000

    pin lighgtning commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url="https://github.com/lightning-AI/lit-gpt",
     install_requires=[
         "torch>=2.1.0",
-        "lightning @ git+https://github.com/Lightning-AI/lightning@71aed751f7f0ca8422ddca256e602099070f490b",
+        "lightning @ git+https://github.com/Lightning-AI/lightning@e59dc41c8eec9fb237ff8ab977cb39d8d7511510",
     ],
     packages=find_packages(),
     long_description=readme,
