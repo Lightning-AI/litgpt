7b4a642df04b41eace2e27e35506e441ed5d40bb
Andrei-Aksionov <aksionau.andrei@gmail.com>
2024-01-12 22:15:05 +0300

    Test_convert_lit: explicitly provide partial_rotary_factor

--- tests/test_convert_lit_checkpoint.py
+++ tests/test_convert_lit_checkpoint.py
@@ -264,6 +264,7 @@ def test_against_hf_phi_1_5():
         intermediate_size=ours_config.intermediate_size,
         num_attention_heads=ours_config.n_head,
         num_hidden_layers=ours_config.n_layer,
+        partial_rotary_factor=ours_config.rotary_percentage,
     )
 
     ours_model = GPT(ours_config)
@@ -313,6 +314,7 @@ def test_against_hf_phi_2():
         intermediate_size=ours_config.intermediate_size,
         num_attention_heads=ours_config.n_head,
         num_hidden_layers=ours_config.n_layer,
+        partial_rotary_factor=ours_config.rotary_percentage,
     )
 
     ours_model = GPT(ours_config)
