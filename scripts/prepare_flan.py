7e4971f32142f3fa4077f5ff09914bbc4dce7822
Carlos Mocholí <carlossmocholi@gmail.com>
2024-01-18 20:49:50 +0100

    Fmt

--- scripts/prepare_flan.py
+++ scripts/prepare_flan.py
@@ -1,13 +1,13 @@
+# Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+
 """Implementation derived from https://github.com/tloen/alpaca-lora"""
 import json
-import os
 import sys
 from pathlib import Path
-import requests
 from typing import Optional
 
+import requests
 import torch
-from torch.utils.data import random_split
 from tqdm import tqdm
 
 # support running without installing as a package
@@ -15,6 +15,7 @@ wd = Path(__file__).parent.parent.resolve()
 sys.path.append(str(wd))
 
 from lit_gpt.tokenizer import Tokenizer
+
 
 def download_if_missing(file_path: Path, file_url: str):
     """Downloads the raw json data file and saves it in the given destination."""
@@ -50,72 +51,72 @@ def prepare(
     """
 
     supported_subsets = {
-        'aeslc_10templates',
-        'ag_news_subset_10templates',
-        'anli_r1_10templates',
-        'anli_r2_10templates',
-        'anli_r3_10templates',
-        'arc_challenge_10templates',
-        'arc_easy_10templates',
-        'bool_q_10templates',
-        'cb_10templates',
-        'cnn_dailymail_10templates',
-        'cola_10templates',
-        'common_gen_10templates',
-        'copa_10templates',
-        'coqa_10templates',
-        'cosmos_qa_10templates',
-        'dart_10templates',
-        'definite_pronoun_resolution_10templates',
-        'drop_10templates',
-        'e2e_nlg_10templates',
-        'fix_punct_10templates',
-        'gigaword_10templates',
-        'glue_mrpc_10templates',
-        'glue_qqp_10templates',
-        'hellaswag_10templates',
-        'imdb_reviews_10templates',
-        'math_dataset_10templates',
-        'mnli_matched_10templates',
-        'mnli_mismatched_10templates',
-        'multi_news_10templates',
-        'multirc_10templates',
-        'natural_questions_10templates',
-        'openbookqa_10templates',
-        'opinion_abstracts_idebate_10templates',
-        'opinion_abstracts_rotten_tomatoes_10templates',
-        'para_crawl_enes_10templates',
-        'paws_wiki_10templates',
-        'piqa_10templates',
-        'qnli_10templates',
-        'quac_10templates',
-        'record_10templates',
-        'rte_10templates',
-        'samsum_10templates',
-        'sentiment140_10templates',
-        'snli_10templates',
-        'squad_v1_10templates',
-        'squad_v2_10templates',
-        'sst2_10templates',
-        'story_cloze_10templates',
-        'stsb_10templates',
-        'trec_10templates',
-        'trivia_qa_10templates',
-        'true_case_10templates',
-        'web_nlg_en_10templates',
-        'wic_10templates',
-        'wiki_lingua_english_en_10templates',
-        'wmt14_enfr_10templates',
-        'wmt16_translate_csen_10templates',
-        'wmt16_translate_deen_10templates',
-        'wmt16_translate_fien_10templates',
-        'wmt16_translate_roen_10templates',
-        'wmt16_translate_ruen_10templates',
-        'wmt16_translate_tren_10templates',
-        'wnli_10templates',
-        'word_segment_10templates',
-        'wsc_10templates',
-        'yelp_polarity_reviews_10templates'
+        "aeslc_10templates",
+        "ag_news_subset_10templates",
+        "anli_r1_10templates",
+        "anli_r2_10templates",
+        "anli_r3_10templates",
+        "arc_challenge_10templates",
+        "arc_easy_10templates",
+        "bool_q_10templates",
+        "cb_10templates",
+        "cnn_dailymail_10templates",
+        "cola_10templates",
+        "common_gen_10templates",
+        "copa_10templates",
+        "coqa_10templates",
+        "cosmos_qa_10templates",
+        "dart_10templates",
+        "definite_pronoun_resolution_10templates",
+        "drop_10templates",
+        "e2e_nlg_10templates",
+        "fix_punct_10templates",
+        "gigaword_10templates",
+        "glue_mrpc_10templates",
+        "glue_qqp_10templates",
+        "hellaswag_10templates",
+        "imdb_reviews_10templates",
+        "math_dataset_10templates",
+        "mnli_matched_10templates",
+        "mnli_mismatched_10templates",
+        "multi_news_10templates",
+        "multirc_10templates",
+        "natural_questions_10templates",
+        "openbookqa_10templates",
+        "opinion_abstracts_idebate_10templates",
+        "opinion_abstracts_rotten_tomatoes_10templates",
+        "para_crawl_enes_10templates",
+        "paws_wiki_10templates",
+        "piqa_10templates",
+        "qnli_10templates",
+        "quac_10templates",
+        "record_10templates",
+        "rte_10templates",
+        "samsum_10templates",
+        "sentiment140_10templates",
+        "snli_10templates",
+        "squad_v1_10templates",
+        "squad_v2_10templates",
+        "sst2_10templates",
+        "story_cloze_10templates",
+        "stsb_10templates",
+        "trec_10templates",
+        "trivia_qa_10templates",
+        "true_case_10templates",
+        "web_nlg_en_10templates",
+        "wic_10templates",
+        "wiki_lingua_english_en_10templates",
+        "wmt14_enfr_10templates",
+        "wmt16_translate_csen_10templates",
+        "wmt16_translate_deen_10templates",
+        "wmt16_translate_fien_10templates",
+        "wmt16_translate_roen_10templates",
+        "wmt16_translate_ruen_10templates",
+        "wmt16_translate_tren_10templates",
+        "wnli_10templates",
+        "word_segment_10templates",
+        "wsc_10templates",
+        "yelp_polarity_reviews_10templates",
     }
 
     if subsets is not None:
@@ -220,12 +221,7 @@ def prepare_sample(example: dict, tokenizer: Tokenizer, max_length: int, mask_in
     if mask_inputs:
         labels[: len(encoded_full_prompt)] = ignore_index
 
-    return {
-        **example,
-        "input_ids": encoded_full_prompt_and_response,
-        "input_ids_no_response": encoded_full_prompt,
-        "labels": labels,
-    }
+    return {**example, "input_ids": encoded_full_prompt_and_response, "labels": labels}
 
 
 def generate_prompt(example):
