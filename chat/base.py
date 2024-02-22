1a2f9f8afbb07b4b860f5e3490415ba6e5e839e5
Andrei-Aksionov <aksionau.andrei@gmail.com>
2024-02-22 20:37:39 +0000

    Prompt for Gemma it (instruct models)

--- chat/base.py
+++ chat/base.py
@@ -361,6 +361,15 @@ def prompt_config(checkpoint_dir: Path, tokenizer: Tokenizer) -> Tuple[str, Tupl
         stop_tokens = ([tokenizer.eos_id],)
         return system_prompt, stop_tokens
 
+    if re.search(r"gemma.*-it", checkpoint_name):
+        system_prompt = (
+            "<start_of_turn>user\n"
+            "{prompt}<end_of_turn>\n"
+            "<start_of_turn>model\n"
+        )
+        stop_tokens = ([tokenizer.eos_id],)
+        return system_prompt, stop_tokens
+
     # default format
     return "{prompt}", ([tokenizer.eos_id],)
 
