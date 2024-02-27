066b7769b62cec81ad44b891e8c855282455b966
awaelchli <aedu.waelchli@gmail.com>
2024-02-27 16:21:15 +0100

    tests

--- lit_gpt/datasets/tinyllama.py
+++ lit_gpt/datasets/tinyllama.py
@@ -29,12 +29,16 @@ class TinyLlama(LitDataModule):
         num_workers: int = 8,
     ) -> None:
         super().__init__()
-        self.data_path = data_path
         self.seed = seed
         self.num_workers = num_workers
 
         self.batch_size = 1
         self.seq_length = 2048
+
+        # Could be a remote path (s3://) or a local path
+        self.slimpajama_train = str(data_path).rstrip("/") + "/slimpajama/train"
+        self.slimpajama_val = str(data_path).rstrip("/") + "/slimpajama/val"
+        self.starcoder_train = str(data_path).rstrip("/") + "/starcoder"
 
     def connect(
         self,
@@ -46,24 +50,25 @@ class TinyLlama(LitDataModule):
         self.seq_length = max_seq_length + 1  # Increase by one because we need the next token as well
 
     def prepare_data(self) -> None:
-        pass
+        for path in (self.slimpajama_train, self.slimpajama_val, self.starcoder_train):
+            if not path.startswith("s3://") and not Path(path).is_dir():
+                raise FileNotFoundError(
+                    "The data path for TinyLlama is expected to be the directory containing these subdirectories:"
+                    f" `slimpajama/train`, `slimpajama/val`, `starcoder`. The directory {path} does not exist."
+                )
 
     def train_dataloader(self) -> DataLoader:
         from lightning.data.streaming import CombinedStreamingDataset, StreamingDataLoader, StreamingDataset, TokensLoader
 
-        # Could be a remote path (s3://) or a local path
-        slimpajama_train = str(self.data_path).rstrip("/") + "/slimpajama/train"
-        starcoder_train = str(self.data_path).rstrip("/") + "/starcoder"
-
         train_datasets = [
             StreamingDataset(
-                input_dir=slimpajama_train,
+                input_dir=self.slimpajama_train,
                 item_loader=TokensLoader(block_size=self.seq_length),
                 shuffle=True,
                 drop_last=True,
             ),
             StreamingDataset(
-                input_dir=starcoder_train,
+                input_dir=self.starcoder_train,
                 item_loader=TokensLoader(block_size=self.seq_length),
                 shuffle=True,
                 drop_last=True,
@@ -81,10 +86,8 @@ class TinyLlama(LitDataModule):
     def val_dataloader(self) -> DataLoader:
         from lightning.data.streaming import StreamingDataset, TokensLoader
 
-        slimpajama_val = str(self.data_path).rstrip("/") + "/slimpajama/val"
-
         val_dataset = StreamingDataset(
-            input_dir=slimpajama_val,
+            input_dir=self.slimpajama_val,
             item_loader=TokensLoader(block_size=self.seq_length),
             shuffle=True,
             # Consider setting to False, but we would lose some samples due to truncation when world size > 1
