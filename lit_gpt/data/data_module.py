bf5d38b303797b4fc5ec31006841aaf55e9b2a6e
awaelchli <aedu.waelchli@gmail.com>
2024-02-28 01:51:31 +0100

    Revert "restore"
    This reverts commit 87c4c0e7de1719c17e0b7079fb9d756e4a6e13db.

--- lit_gpt/data/data_module.py
+++ lit_gpt/data/data_module.py
@@ -1,10 +1,6 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
 from abc import abstractmethod
-from typing import List, Dict, Union, Optional
-
-import torch
-from torch import Tensor
-from torch.utils.data import Dataset
+from typing import Optional
 
 from lightning import LightningDataModule
 from lit_gpt import Tokenizer
@@ -27,92 +23,3 @@ class LitDataModule(LightningDataModule):
     def setup(self, stage: str = "") -> None:
         # Stub is to redefine the default signature, because the concept of 'stage' does not exist in Lit-GPT
         pass
-
-
-class SFTDataset(Dataset):
-    """An in-memory dataset for supervised finetuning with `input_ids` and `labels`.
-
-    Args:
-        data: A list of samples (dicts). The target/label must be stored under the key 'output' and the instruction
-            or other data can be stored under any key as long as it is compatible with the given prompt template.
-        tokenizer: The tokenizer to use. Should match the one that was used to pretrain the model.
-        prompt_template: A prompt template (format string or callable)
-        max_seq_length: Truncate sequences that are longer than this value. By default, no truncation is applied.
-        mask_prompt: Whether to mask the prompt section from the label (with ``ignore_index``)
-        ignore_index: The index to use for elements to be ignored in the label.
-
-    Returns a dict with two keys:
-        input_ids: The encoded prompt + response
-        labels: Same as input_ids, unless ``mask_prompt=True`` in which case the 'prompt' part is replaced with
-            the ``ignore_index``.
-    """
-    def __init__(
-        self,
-        data: List[Dict[str, str]],
-        tokenizer: Tokenizer,
-        prompt_template: Union[str, callable],
-        max_seq_length: int = -1,
-        mask_prompt: bool = True,
-        ignore_index: int = -1,
-    ) -> None:
-        self.data = data
-        self.tokenizer = tokenizer
-        self.prompt_template = prompt_template
-        self.max_seq_length = max_seq_length
-        self.mask_prompt = mask_prompt
-        self.ignore_index = ignore_index
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __getitem__(self, idx: int) -> Dict[str, Tensor]:
-        example = self.data[idx]
-        prompt = apply_prompt_template(self.prompt_template, example)
-        prompt_and_response = prompt + example["output"]
-        encoded_prompt = self.tokenizer.encode(prompt, max_length=self.max_seq_length)
-        encoded_prompt_and_response = self.tokenizer.encode(
-            prompt_and_response,
-            eos=True,
-            max_length=self.max_seq_length,
-        )
-
-        # The labels are the full prompt with response, but with the prompt masked out
-        labels = encoded_prompt_and_response.clone()
-        if self.mask_prompt:
-            labels[: len(encoded_prompt)] = self.ignore_index
-
-        return {"input_ids": encoded_prompt_and_response.type(torch.int64), "labels": labels.type(torch.int64)}
-
-
-def sft_collate_fn(samples: List[Dict[str, Tensor]], max_seq_length: int = -1, pad_id: int = 0, ignore_index: int = -1) -> Dict[str, Tensor]:
-    """Collate function for supervised finetuning with.
-
-    Gets a list of dicts with keys `input_ids` and `labels`. Returns a dict with batched `input_ids` and `labels`.
-    Pads short sequences to the longest element in the batch. Optionally truncates all sequences to the specified
-    maximum length.
-    """
-    longest = max(len(sample["input_ids"]) for sample in samples)
-    max_length = max_seq_length if max_seq_length > 0 else longest
-
-    batched = {}
-    for key in ("input_ids", "labels"):
-        pad_value = pad_id if key == "input_ids" else ignore_index
-
-        # Pad right based on the longest sequence
-        batched[key] = torch.stack([
-            torch.nn.functional.pad(sample[key], (0, longest - len(sample[key])), value=pad_value)
-            for sample in samples
-        ])
-
-        # Truncate if needed
-        batched[key] = batched[key][:, :max_length]
-
-    return batched
-
-
-def apply_prompt_template(template: Union[str, callable], example: Dict[str, str]) -> str:
-    if isinstance(template, str):
-        prompt = template.format(**example)
-    else:
-        prompt = template(example)
-    return prompt
