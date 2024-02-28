522c15f8ed64b0a69f72b5bf2d1e22dcd83158dc
awaelchli <aedu.waelchli@gmail.com>
2024-02-28 14:18:40 +0100

    test dolly

--- tests/data/conftest.py
+++ tests/data/conftest.py
@@ -9,3 +9,10 @@ def alpaca_path(tmp_path):
     file = Path(__file__).parent / "fixtures" / "alpaca.json"
     shutil.copyfile(file, tmp_path / "alpaca.json")
     return tmp_path / "alpaca.json"
+
+
+@pytest.fixture()
+def dolly_path(tmp_path):
+    file = Path(__file__).parent / "fixtures" / "dolly.json"
+    shutil.copyfile(file, tmp_path / "dolly.json")
+    return tmp_path / "dolly.json"
