c990ce54f9f7db8f668ca8aed6ebdc8ed3c0201a
awaelchli <aedu.waelchli@gmail.com>
2024-02-28 02:56:06 +0100

    test

--- tests/data/test_base.py
+++ tests/data/test_base.py
@@ -7,32 +7,43 @@ import torch
 
 class MockTokenizer:
     """A dummy tokenizer that encodes each character as its ASCII code."""
-    def encode(self, text, eos=False, **kwargs):
+    def encode(self, text, eos=False, max_length=-1):
         output = [ord(c) for c in text]
         if eos:
             output.append(1)
+        output = output[:max_length] if max_length > 0 else output
         return torch.tensor(output)
 
 
 @pytest.mark.parametrize("mask_prompt", [True, False])
-def test_sft_dataset(mask_prompt):
+@pytest.mark.parametrize("ignore_index", [-1, -100])
+@pytest.mark.parametrize("max_seq_length", [1000, 5])
+def test_sft_dataset(max_seq_length, ignore_index, mask_prompt):
     from lit_gpt.data import SFTDataset
 
+    i = ignore_index
     prompt_template = "In: {instruction} Out:"
     data = [
         {"instruction": "Foo", "output": "Bar"},
         {"instruction": "Boo", "output": "Ahh"},
     ]
 
-    dataset = SFTDataset(data=data, tokenizer=MockTokenizer(), prompt_template=prompt_template, mask_prompt=mask_prompt)
-    expected = {
-        "input_ids": torch.tensor([73, 110, 58, 32, 70, 111, 111, 32, 79, 117, 116, 58, 66, 97, 114, 1]),
-        "labels": torch.tensor([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 66, 97, 114, 1]),
-    }
+    dataset = SFTDataset(
+        data=data,
+        tokenizer=MockTokenizer(),
+        prompt_template=prompt_template,
+        mask_prompt=mask_prompt,
+        ignore_index=ignore_index,
+        max_seq_length=max_seq_length,
+    )
+    assert len(dataset) == len(data)
 
-    assert torch.equal(dataset[0]["input_ids"], expected["input_ids"])
+    expected_input_ids = torch.tensor([73, 110, 58, 32, 70, 111, 111, 32, 79, 117, 116, 58, 66, 97, 114, 1])
     # If prompt is not masked, labels == input_ids
-    assert torch.equal(dataset[0]["labels"], expected["labels"] if mask_prompt else expected["input_ids"])
+    expected_labels = torch.tensor([i, i, i, i, i, i, i, i, i, i, i, i, 66, 97, 114, 1]) if mask_prompt else expected_input_ids
+
+    assert torch.equal(dataset[0]["input_ids"], expected_input_ids[:max_seq_length])
+    assert torch.equal(dataset[0]["labels"], expected_labels[:max_seq_length])
 
 
 @pytest.mark.parametrize("ignore_index", [-1, -100])
