f814cadd8c143869cefff1ab57cc1b812f2e49d8
awaelchli <aedu.waelchli@gmail.com>
2024-02-29 19:38:42 +0100

    (4/n) Data Refactor - Finetuning Scripts (#950)
    Co-authored-by: rasbt <mail@sebastianraschka.com>
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
 
 import os
+import shutil
 import sys
 from pathlib import Path
 from typing import List
@@ -72,6 +73,30 @@ class MockTokenizer:
 @pytest.fixture()
 def mock_tockenizer():
     return MockTokenizer()
+
+
+@pytest.fixture()
+def alpaca_path(tmp_path):
+    file = Path(__file__).parent / "data" / "fixtures" / "alpaca.json"
+    shutil.copyfile(file, tmp_path / "alpaca.json")
+    return tmp_path / "alpaca.json"
+
+
+@pytest.fixture()
+def dolly_path(tmp_path):
+    file = Path(__file__).parent / "data" / "fixtures" / "dolly.json"
+    shutil.copyfile(file, tmp_path / "dolly.json")
+    return tmp_path / "dolly.json"
+
+
+@pytest.fixture()
+def longform_path(tmp_path):
+    path = tmp_path / "longform"
+    path.mkdir()
+    for split in ("train", "val"):
+        file = Path(__file__).parent / "data" / "fixtures" / f"longform_{split}.json"
+        shutil.copyfile(file, path / f"{split}.json")
+    return path
 
 
 def RunIf(**kwargs):
