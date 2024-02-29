fcc74bc62b07e282adb665edfd4f35da609aa96e
awaelchli <aedu.waelchli@gmail.com>
2024-02-29 13:24:56 +0100

    (3/n) Data Refactor - Add JSON Module (#971)

--- lit_gpt/data/json.py
+++ lit_gpt/data/json.py
@@ -2,7 +2,7 @@
 
 import json
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional
 
 import torch
 from torch.utils.data import random_split, DataLoader
@@ -11,16 +11,26 @@ from lit_gpt.data.alpaca import prompt_template
 from lit_gpt.tokenizer import Tokenizer
 
 
-class CSV(LitDataModule):
-    """Loads CSV data for supervised finetuning.
+class JSON(LitDataModule):
+    """Loads JSON data for supervised finetuning.
 
     Provides train- and val-dataloaders. The batches return keys "input_ids" and "labels".
+
+    Args:
+        json_path: A path to a JSON file containing the data. The file should contain a list of samples (dicts).
+            Each dict must have the keys 'instruction' and 'output', and can optionally have a key 'input'
+            (see Alpaca).
+        mask_prompt: Whether to mask the prompt section from the label (with ``ignore_index``).
+        test_split_fraction: A number in the range [0, 1] that determines the fraction of the dataset
+            to use for testing.
+        ignore_index: The index to use for elements to be ignored in the label.
+        seed: The random seed for creating the train/val splits and shuffling the dataset.
+        num_workers: How many DataLoader processes to use for loading.
     """
 
     def __init__(
         self,
-        csv_path: Path,
-        columns: Tuple[str, ...] = ("instruction", "input", "output"),
+        json_path: Path,
         mask_prompt: bool = False,
         test_split_fraction: float = 0.1,
         ignore_index: int = -1,
@@ -28,8 +38,7 @@ class CSV(LitDataModule):
         num_workers: int = 4,
     ) -> None:
         super().__init__()
-        self.csv_path = csv_path
-        self.columns = columns
+        self.json_path = json_path
         self.mask_prompt = mask_prompt
         self.test_split_fraction = test_split_fraction
         self.ignore_index = ignore_index
@@ -42,6 +51,9 @@ class CSV(LitDataModule):
         self.train_dataset: Optional[SFTDataset] = None
         self.test_dataset: Optional[SFTDataset] = None
 
+        if not self.json_path.is_file():
+            raise FileNotFoundError(f"The file {self.json_path} does not exist.")
+
     def connect(
         self,
         tokenizer: Optional[Tokenizer] = None,
@@ -53,12 +65,8 @@ class CSV(LitDataModule):
         self.max_seq_length = -1 if max_seq_length is None else max_seq_length
 
     def setup(self, stage: str = "") -> None:
-        import pandas as pd
-
-        df = pd.read_csv(self.csv_path, dtype=str).fillna("")
-        if not (df.columns.values == self.columns).all():
-            raise ValueError(f"CSV columns must be {self.columns}, found {df.columns.values}")
-        data = json.loads(df.to_json(orient="records", indent=4))
+        with open(self.json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
 
         # Partition the dataset into train and test
         train_data, test_data = random_split(
