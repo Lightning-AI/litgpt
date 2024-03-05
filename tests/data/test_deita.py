907be5cc60b89e34c087321761a42d151973dffb
awaelchli <aedu.waelchli@gmail.com>
2024-03-05 02:41:15 +0100

    update test

--- tests/data/test_deita.py
+++ tests/data/test_deita.py
@@ -1,25 +1,93 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+from unittest import mock
 
-def test_deita(mock_tockenizer, deita_path):
-    from lit_gpt.data import Deita
+
+def test_format_dataset():
+    from lit_gpt.data.deita import format_dataset
+
+    data = [
+        {
+            "prompt": "prompt1",
+            "prompt_id": "1",
+            "messages": [
+                {"content": "question1", "role": "user"},
+                {"content": "response1", "role": "assistant"},
+                {"content": "question2", "role": "user"},
+                {"content": "response2", "role": "assistant"},
+            ],
+        },
+        {
+            "prompt": "prompt2",
+            "prompt_id": "2",
+            "messages": [
+                {"content": "question3", "role": "user"},
+                {"content": "response3", "role": "assistant"},
+                {"content": "question4", "role": "user"},
+                {"content": "response4", "role": "assistant"},
+            ],
+        },
+    ]
+
+    assert format_dataset(data, include_multi_turn_conversations=False) == [
+        {"instruction": "question1", "output": "response1", "input": ""},
+        {"instruction": "question3", "output": "response3", "input": ""},
+    ]
+    assert format_dataset(data, include_multi_turn_conversations=True) == [
+        {"instruction": "question1", "output": "response1", "input": ""},
+        {"instruction": "question2", "output": "response2", "input": ""},
+        {"instruction": "question3", "output": "response3", "input": ""},
+        {"instruction": "question4", "output": "response4", "input": ""},
+    ]
+
+
+@mock.patch("lit_gpt.data.deita.format_dataset")
+@mock.patch("datasets.load_dataset")
+def test_deita(_, format_dataset_mock, mock_tockenizer, tmp_path):
+    from lit_gpt.data import Deita, SFTDataset
+
+    format_dataset_mock.return_value = [
+        {"instruction": "inst1", "output": "out1"},
+        {"instruction": "inst2", "output": "out2"},
+        {"instruction": "inst3", "output": "out3"},
+    ]
 
     deita = Deita(
-        repo_id=deita_path.name + "/",
         num_workers=0,
+        download_dir=tmp_path,
     )
     deita.connect(mock_tockenizer, batch_size=2, max_seq_length=10)
     deita.prepare_data()
     deita.setup()
 
     train_dataloader = deita.train_dataloader()
+    assert isinstance(train_dataloader.dataset, SFTDataset)
+    assert len(train_dataloader) == 2
+
     val_dataloader = deita.val_dataloader()
+    assert isinstance(val_dataloader.dataset, SFTDataset)
+    assert len(val_dataloader) == 2
 
-    assert len(train_dataloader) == 6
-    assert len(val_dataloader) == 6
 
-    train_batch = next(iter(train_dataloader))
-    val_batch = next(iter(val_dataloader))
-
-    assert train_batch.keys() == val_batch.keys() == {"input_ids", "labels"}
-    assert all(seq.shape == (2, 10) for seq in train_batch.values())
-    assert all(seq.shape == (2, 10) for seq in val_batch.values())
+# def test_deita(mock_tockenizer, deita_path):
+#     from lit_gpt.data import Deita
+#
+#     deita = Deita(
+#         repo_id=deita_path.name + "/",
+#         num_workers=0,
+#     )
+#     deita.connect(mock_tockenizer, batch_size=2, max_seq_length=10)
+#     deita.prepare_data()
+#     deita.setup()
+#
+#     train_dataloader = deita.train_dataloader()
+#     val_dataloader = deita.val_dataloader()
+#
+#     assert len(train_dataloader) == 6
+#     assert len(val_dataloader) == 6
+#
+#     train_batch = next(iter(train_dataloader))
+#     val_batch = next(iter(val_dataloader))
+#
+#     assert train_batch.keys() == val_batch.keys() == {"input_ids", "labels"}
+#     assert all(seq.shape == (2, 10) for seq in train_batch.values())
+#     assert all(seq.shape == (2, 10) for seq in val_batch.values())
