18b74bcd2d81aabe2383ac3a439ed10270236cad
Sebastian Raschka <mail@sebastianraschka.com>
2024-03-04 19:12:59 -0600

    Copy configs to new checkpoint dir (#984)
    Co-authored-by: awaelchli <aedu.waelchli@gmail.com>

--- finetune/adapter_v2.py
+++ finetune/adapter_v2.py
@@ -31,7 +31,8 @@ from lit_gpt.utils import (
     load_checkpoint,
     num_parameters,
     CycleIterator,
-    parse_devices
+    parse_devices,
+    copy_config_files,
 )
 
 
@@ -139,8 +140,11 @@ def main(fabric: L.Fabric, devices: int, seed: int, config: Config, data: LitDat
         fabric.print(f"Memory used: {torch.cuda.max_memory_allocated() / 1e9:.02f} GB")
 
     # Save the final checkpoint at the end of training
-    save_path = out_dir / "lit_model_adapter_finetuned.pth"
+    save_path = out_dir / "final" / "lit_model.pth"
+    save_path.parent.mkdir(parents=True, exist_ok=True)
     save_adapter_v2_checkpoint(fabric, model, save_path)
+    # Copy checkpoint files from original checkpoint dir
+    copy_config_files(checkpoint_dir, save_path.parent)
 
 
 def fit(
@@ -215,8 +219,10 @@ def fit(
             fabric.print(f"iter {iter_num}: val loss {val_loss.item():.4f}, val time: {t1 * 1000:.2f} ms")
             fabric.barrier()
         if not is_accumulating and step_count % train.save_interval == 0:
-            checkpoint_path = out_dir / f"iter-{iter_num:06d}-ckpt.pth"
-            save_adapter_v2_checkpoint(fabric, model, checkpoint_path)
+            checkpoint_file = out_dir / f"iter-{iter_num:06d}" / "lit_model.pth"
+            checkpoint_file.parent.mkdir(parents=True, exist_ok=True)
+            save_adapter_v2_checkpoint(fabric, model, checkpoint_file)
+            copy_config_files(checkpoint_dir, checkpoint_file.parent)
 
 
 # the adapter "kv cache" cannot be initialized under `inference_mode`
