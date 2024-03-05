c4eb9d8e177c8c12ac1024dd1fe6b5b86a2569ed
awaelchli <aedu.waelchli@gmail.com>
2024-03-05 14:59:20 +0100

    add trivial tests

---
+++ tests/test_prompts.py
@@ -0,0 +1,17 @@
+# Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+
+
+def test_default_prompt_style(mock_tockenizer):
+    from lit_gpt.prompts import Default
+
+    prompt_style = Default()
+    prompt = "This is a test prompt."
+    assert prompt_style.apply(prompt) == prompt
+    assert prompt_style.stop_tokens(mock_tockenizer) == ([mock_tockenizer.eos_id], )
+
+
+def test_prompt_style_from_name():
+    from lit_gpt.prompts import PromptStyle, prompt_styles
+
+    for style_name in prompt_styles:
+        assert isinstance(PromptStyle.from_name(style_name), prompt_styles[style_name])
