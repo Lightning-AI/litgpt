ab32934b963dafdfecc488306a819f72e9c4b500
awaelchli <aedu.waelchli@gmail.com>
2024-03-05 15:04:18 +0100

    remove old tests

--- tests/data/test_deita.py
+++ tests/data/test_deita.py
@@ -66,28 +66,3 @@ def test_deita(_, format_dataset_mock, mock_tockenizer, tmp_path):
     val_dataloader = deita.val_dataloader()
     assert isinstance(val_dataloader.dataset, SFTDataset)
     assert len(val_dataloader) == 2
-
-
-# def test_deita(mock_tockenizer, deita_path):
-#     from lit_gpt.data import Deita
-#
-#     deita = Deita(
-#         repo_id=deita_path.name + "/",
-#         num_workers=0,
-#     )
-#     deita.connect(mock_tockenizer, batch_size=2, max_seq_length=10)
-#     deita.prepare_data()
-#     deita.setup()
-#
-#     train_dataloader = deita.train_dataloader()
-#     val_dataloader = deita.val_dataloader()
-#
-#     assert len(train_dataloader) == 6
-#     assert len(val_dataloader) == 6
-#
-#     train_batch = next(iter(train_dataloader))
-#     val_batch = next(iter(val_dataloader))
-#
-#     assert train_batch.keys() == val_batch.keys() == {"input_ids", "labels"}
-#     assert all(seq.shape == (2, 10) for seq in train_batch.values())
-#     assert all(seq.shape == (2, 10) for seq in val_batch.values())
