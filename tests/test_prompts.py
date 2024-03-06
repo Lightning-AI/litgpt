ebcb6a07f07429035fd79d1ada4a29bd178e5b93
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-03-06 03:27:57 +0100

    add requested test

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -23,3 +23,16 @@ def test_prompt_style_from_config():
 
     for model_name, style_name in model_name_to_prompt_style.items():
         assert isinstance(PromptStyle.from_config(Config.from_name(model_name)), prompt_styles[style_name])
+
+
+def test_apply_promts():
+    from lit_gpt.prompts import prompt_styles, Alpaca
+
+    prompt = "Is a coconut a nut or a fruit?"
+    inp = "Optional input"
+
+    for style in prompt_styles.values():
+        output = style().apply(prompt, input=inp)
+        assert prompt in output
+        if isinstance(style, Alpaca):
+            assert inp in output
