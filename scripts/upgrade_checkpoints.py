73aed7c8dbc41925f896e2c182a9a3af557fe997
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-03-06 03:34:32 -0500

    update

--- scripts/upgrade_checkpoints.py
+++ scripts/upgrade_checkpoints.py
@@ -17,18 +17,18 @@ def upgrade(root_dir: Path = Path("."), write: bool = False) -> False:
     for filepath in config_files:
         with open(filepath, "r") as file:
             config = json.load(file)
-        updated_config = config.clone()
+        updated_config = config.copy()
 
         if "name" in config and "org" in config and not "hf_config" in config:
             name = updated_config["name"]
             org = updated_config.pop("org")
             updated_config["hf_config"] = {"name": name, "org": org}
         if "_norm_class" in config:
-            updated_config["norm_class_name"] = config.pop("_norm_class")
+            updated_config["norm_class_name"] = updated_config.pop("_norm_class")
         if "_mlp_class" in config:
-            updated_config["mlp_class_name"] = config.pop("_mlp_class")
+            updated_config["mlp_class_name"] = updated_config.pop("_mlp_class")
         if "condense_ratio" in config:
-            updated_config["rope_condense_ratio"] = config.pop("condense_ratio")
+            updated_config["rope_condense_ratio"] = updated_config.pop("condense_ratio")
 
         if write:
             with open(filepath, "w") as file:
@@ -40,7 +40,7 @@ def upgrade(root_dir: Path = Path("."), write: bool = False) -> False:
         print("The following files will be updated:\n")
         for file in files_to_update:
             print(file)
-        print("\nRerun the previous command with `--write` to write the changes to all these files.")
+        print("\nRerun the previous command with `--write True` to write the changes to all these files.")
 
 
 if __name__ == "__main__":
