20044a3f213c75a103612609617ffffd9ba3c40a
Carlos Mocholí <carlossmocholi@gmail.com>
2024-03-07 01:59:20 +0100

    Update lit_gpt/data/alpaca_2k.py
    Co-authored-by: awaelchli <aedu.waelchli@gmail.com>

--- lit_gpt/data/alpaca_2k.py
+++ lit_gpt/data/alpaca_2k.py
@@ -30,7 +30,7 @@ class Alpaca2k(Alpaca):
 
         dataset = load_dataset(self.repo_id, cache_dir=self.download_dir)
 
-        train_validation_split = dataset["train"].train_test_split(test_size=self.test_split_fraction, seed=42)
+        train_validation_split = dataset["train"].train_test_split(test_size=self.test_split_fraction, seed=self.seed)
         train_data = train_validation_split["train"]
         test_data = train_validation_split["test"]
 
