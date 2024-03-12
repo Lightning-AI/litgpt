17e8ec2f416ccb8b96853bbae6ac1e3c15381072
Carlos Mocholí <carlossmocholi@gmail.com>
2024-03-12 04:10:19 +0100

    Support URLs for configs (#1091)

--- tests/test_config_hub.py
+++ tests/test_config_hub.py
@@ -11,6 +11,7 @@ import pytest
     ("litgpt/pretrain.py", "pretrain/debug.yaml"),
     ("litgpt/pretrain.py", "pretrain/tinyllama.yaml"),
     ("litgpt/pretrain.py", "pretrain/tinystories.yaml"),
+    ("litgpt/pretrain.py", "https://raw.githubusercontent.com/Lightning-AI/litgpt/wip/config_hub/pretrain/tinystories.yaml"),
     ("litgpt/finetune/full.py", "finetune/llama-2-7b/full.yaml"),
     ("litgpt/finetune/lora.py", "finetune/llama-2-7b/lora.yaml"),
     ("litgpt/finetune/lora.py", "finetune/tiny-llama/lora.yaml"),
@@ -20,10 +21,10 @@ def test_config_help(script_file, config_file, monkeypatch, tmp_path):
     from litgpt.utils import CLI
 
     script_file = Path(__file__).parent.parent / script_file
-    config_file = Path(__file__).parent.parent / "config_hub" / config_file
-
     assert script_file.is_file()
-    assert config_file.is_file()
+    if "http" not in str(config_file):
+        config_file = Path(__file__).parent.parent / "config_hub" / config_file
+        assert config_file.is_file()
 
     spec = importlib.util.spec_from_file_location(str(script_file.parent.name), script_file)
     module = importlib.util.module_from_spec(spec)
