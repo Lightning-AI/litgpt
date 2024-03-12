fc05b00a3bd69b22ec2a0a59f9f92c7b3180dc24
Carlos Mocholí <carlossmocholi@gmail.com>
2024-03-12 02:27:38 +0100

    Fancier

--- litgpt/__main__.py
+++ litgpt/__main__.py
@@ -4,73 +4,16 @@ import torch
 
 
 def main():
-    from jsonargparse import ArgumentParser, Namespace
+    from jsonargparse import ArgumentParser
 
-    # root parser
-    parser = ArgumentParser(prog="litgpt")
-
-    # level 1 subcommands
-
-    pretrain = ArgumentParser()
     from litgpt.pretrain import setup as pretrain_fn
-    pretrain.add_function_arguments(pretrain_fn)
-
-    finetune = ArgumentParser()
-
-    generate = ArgumentParser()
-
-    chat = ArgumentParser()
     from litgpt.chat.base import main as chat_fn
-    chat.add_function_arguments(chat_fn)
-
-    convert = ArgumentParser()
-
-    download = ArgumentParser()
     from litgpt.scripts.download import download as download_fn
-    download.add_function_arguments(download_fn)
-
-    merge_lora = ArgumentParser()
     from litgpt.scripts.merge_lora import merge_lora as merge_lora_fn
-    merge_lora.add_function_arguments(merge_lora_fn)
-
-    subcommands = parser.add_subcommands()
-    subcommands.add_subcommand("pretrain", pretrain, help="Pretrain a model.")
-    subcommands.add_subcommand("finetune", finetune, help="Finetune a model with one of our existing methods.")
-    subcommands.add_subcommand("generate", generate, help="Generate text samples based on a model and tokenizer.")
-    subcommands.add_subcommand("chat", chat, help="Chat with a model.")
-    subcommands.add_subcommand("convert", convert, help="Utilities to convert from and to LitGPT.")
-    subcommands.add_subcommand("download", download, help="Download weights or tokenizer data from the Hugging Face Hub.")
-    # TODO: should this be at the first level?
-    subcommands.add_subcommand("merge_lora", merge_lora, help="Merges the LoRA weights with the base model.")
-
-    # level 2 subcommands
-
-    finetune_subcommands = finetune.add_subcommands()
-    finetune_lora = ArgumentParser()
-    finetune_full = ArgumentParser()
-    finetune_adapter = ArgumentParser()
-    finetune_adapter_v2 = ArgumentParser()
     from litgpt.finetune.lora import setup as finetune_lora_fn
     from litgpt.finetune.full import setup as finetune_full_fn
-    from litgpt.finetune.adapter import setup as finetune_adapter_vn
+    from litgpt.finetune.adapter import setup as finetune_adapter_fn
     from litgpt.finetune.adapter_v2 import setup as finetune_adapter_v2_fn
-    finetune_lora.add_function_arguments(finetune_lora_fn)
-    finetune_full.add_function_arguments(finetune_full_fn)
-    finetune_adapter.add_function_arguments(finetune_adapter_vn)
-    finetune_adapter_v2.add_function_arguments(finetune_adapter_v2_fn)
-    finetune_subcommands.add_subcommand("lora", finetune_lora, help="Finetune a model with LoRA.")
-    finetune_subcommands.add_subcommand("full", finetune_full, help="Finetune a model.")
-    finetune_subcommands.add_subcommand("adapter", finetune_adapter, help="Finetune a model with Adapter.")
-    finetune_subcommands.add_subcommand("adapter_v2", finetune_adapter_v2, help="Finetune a model with Adapter v2.")
-
-    generate_subcommands = generate.add_subcommands()
-    generate_base = ArgumentParser()
-    generate_full = ArgumentParser()
-    generate_lora = ArgumentParser()
-    generate_adapter = ArgumentParser()
-    generate_adapter_v2 = ArgumentParser()
-    generate_sequentially = ArgumentParser()
-    generate_tp = ArgumentParser()
     from litgpt.generate.base import main as generate_base_fn
     from litgpt.generate.full import main as generate_full_fn
     from litgpt.generate.lora import main as generate_lora_fn
@@ -78,70 +21,95 @@ def main():
     from litgpt.generate.adapter_v2 import main as generate_adapter_v2_fn
     from litgpt.generate.sequentially import main as generate_sequentially_fn
     from litgpt.generate.tp import main as generate_tp_fn
-    generate_base.add_function_arguments(generate_base_fn)
-    generate_full.add_function_arguments(generate_full_fn)
-    generate_lora.add_function_arguments(generate_lora_fn)
-    generate_adapter.add_function_arguments(generate_adapter_fn)
-    generate_adapter_v2.add_function_arguments(generate_adapter_v2_fn)
-    generate_sequentially.add_function_arguments(generate_sequentially_fn)
-    generate_tp.add_function_arguments(generate_tp_fn)
-    generate_subcommands.add_subcommand("base", generate_base, help="Default generation option.")
-    generate_subcommands.add_subcommand("full", generate_full, help="For models finetuned with `litgpt finetune full`.")
-    generate_subcommands.add_subcommand("lora", generate_lora, help="For models finetuned with `litgpt finetune lora`.")
-    generate_subcommands.add_subcommand("adapter", generate_adapter, help="For models finetuned with `litgpt finetune adapter`.")
-    generate_subcommands.add_subcommand("adapter_v2", generate_adapter_v2, help="For models finetuned with `litgpt finetune adapter_v2`.")
-    generate_subcommands.add_subcommand("sequentially", generate_sequentially, help="Generation script that partitions layers across devices to be run sequentially.")
-    generate_subcommands.add_subcommand("tp", generate_tp, help="Generation script that uses tensor parallelism to run across devices.")
-
-    convert_subcommands = convert.add_subcommands()
-    convert_pretrained_checkpoint = ArgumentParser()
-    convert_hf_checkpoint = ArgumentParser()
-    convert_lit_checkpoint = ArgumentParser()
-    from litgpt.scripts.convert_pretrained_checkpoint import convert_pretrained_checkpoint as convert_pretrained_checkpoint_fn
+    from litgpt.scripts.convert_pretrained_checkpoint import (
+        convert_pretrained_checkpoint as convert_pretrained_checkpoint_fn,
+    )
     from litgpt.scripts.convert_hf_checkpoint import convert_hf_checkpoint as convert_hf_checkpoint_fn
     from litgpt.scripts.convert_lit_checkpoint import convert_lit_checkpoint as convert_lit_checkpoint_fn
-    convert_pretrained_checkpoint.add_function_arguments(convert_pretrained_checkpoint_fn)
-    convert_hf_checkpoint.add_function_arguments(convert_hf_checkpoint_fn)
-    convert_lit_checkpoint.add_function_arguments(convert_lit_checkpoint_fn)
-    convert_subcommands.add_subcommand("to_litgpt", convert_hf_checkpoint, help="Convert Hugging Face weights to LitGPT weights.")
-    convert_subcommands.add_subcommand("from_litgpt", convert_lit_checkpoint, help="Convert LitGPT weights to Hugging Face weights.")
-    convert_subcommands.add_subcommand("pretrained_checkpoint", convert_pretrained_checkpoint, help="Convert a checkpoint after pretraining.")
 
-    args = parser.parse_args()
-    args_init = parser.instantiate_classes(args)
-    print(args)
-    print(args_init)
+    parser_data = {
+        "pretrain": {"help": "Pretrain a model.", "fn": pretrain_fn},
+        "finetune": {
+            "help": "Finetune a model with one of our existing methods.",
+            "lora": {"help": "Finetune a model with LoRA.", "fn": finetune_lora_fn},
+            "full": {"help": "Finetune a model.", "fn": finetune_full_fn},
+            "adapter": {"help": "Finetune a model with Adapter.", "fn": finetune_adapter_fn},
+            "adapter_v2": {"help": "Finetune a model with Adapter v2.", "fn": finetune_adapter_v2_fn},
+        },
+        "generate": {
+            "help": "Generate text samples based on a model and tokenizer.",
+            "base": {"fn": generate_base_fn, "help": "Default generation option."},
+            "full": {"fn": generate_full_fn, "help": "For models finetuned with `litgpt finetune full`."},
+            "lora": {"fn": generate_lora_fn, "help": "For models finetuned with `litgpt finetune lora`."},
+            "adapter": {"fn": generate_adapter_fn, "help": "For models finetuned with `litgpt finetune adapter`."},
+            "adapter_v2": {
+                "fn": generate_adapter_v2_fn,
+                "help": "For models finetuned with `litgpt finetune adapter v2`.",
+            },
+            "sequentially": {
+                "fn": generate_sequentially_fn,
+                "help": "Generation script that partitions layers across devices to be run sequentially.",
+            },
+            "tp": {
+                "fn": generate_tp_fn,
+                "help": "Generation script that uses tensor parallelism to run across devices.",
+            },
+        },
+        "chat": {"help": "Chat with a model.", "fn": chat_fn},
+        "convert": {
+            "help": "Utilities to convert from and to LitGPT.",
+            "to_litgpt": {"fn": convert_hf_checkpoint_fn, "help": "Convert Hugging Face weights to LitGPT weights."},
+            "from_litgpt": {"fn": convert_lit_checkpoint_fn, "help": "Convert LitGPT weights to Hugging Face weights."},
+            "pretrained_checkpoint": {
+                "fn": convert_pretrained_checkpoint_fn,
+                "help": "Convert a checkpoint after pretraining.",
+            },
+        },
+        "download": {"help": "Download weights or tokenizer data from the Hugging Face Hub.", "fn": download_fn},
+        # TODO: should this be at the first level?
+        "merge_lora": {"help": "Merges the LoRA weights with the base model.", "fn": merge_lora_fn},
+    }
 
+    root_parser = ArgumentParser(prog="litgpt")
 
-    subcommand = args_init.get("subcommand")
-    subargs = args_init.get(subcommand)
+    # register level 1 subcommands and level 2 subsubcommands. If there are more levels in the future we would want to
+    # refactor this to do BFS traversal for registration
+    subcommands = root_parser.add_subcommands()
+    subcommand_to_parser = {}
+    for k, v in parser_data.items():
+        subcommand_parser = ArgumentParser()
+        if "fn" in v:
+            subcommand_parser.add_function_arguments(v["fn"])
+        else:
+            subcommand_to_parser[k] = subcommand_parser
+        subcommands.add_subcommand(k, subcommand_parser, help=v["help"])
+    for subcommand, parser in subcommand_to_parser.items():
+        subcommands = parser.add_subcommands()
+        for k, v in parser_data[subcommand].items():
+            if k == "help":
+                continue
+            subsubcommand_parser = ArgumentParser()
+            subsubcommand_parser.add_function_arguments(v["fn"])
+            subcommands.add_subcommand(k, subsubcommand_parser, help=v["help"])
+
+    args = root_parser.instantiate_classes(root_parser.parse_args())
+
+    subcommand = args.get("subcommand")
+    subargs = args.get(subcommand)
     subsubcommand = subargs.get("subcommand")
     subsubargs = subargs.get(subsubcommand) if isinstance(subsubcommand, str) else None
 
-    subcommands.add_subcommand("pretrain", pretrain, help="Pretrain a model.")
-    subcommands.add_subcommand("finetune", finetune, help="Finetune a model with one of our existing methods.")
-    subcommands.add_subcommand("generate", generate, help="Generate text samples based on a model and tokenizer.")
-    subcommands.add_subcommand("chat", chat, help="Chat with a model.")
-    subcommands.add_subcommand("convert", convert, help="Utilities to convert from and to LitGPT.")
-    subcommands.add_subcommand("download", download, help="Download weights or tokenizer data from the Hugging Face Hub.")
-    # TODO: should this be at the first level?
-    subcommands.add_subcommand("merge_lora", merge_lora, help="Merges the LoRA weights with the base model.")
-
-    subcommands_to_fn = {
-        #"pretrain": {"fn": pretrain_fn, "help": "Pretrain a model."},
-        #"finetune": {
-        #    "lora": finetune_lora_fn
-        #}
-    }
+    level_1 = parser_data[subcommand]
+    if subsubcommand is None:
+        fn = level_1["fn"]
+        kwargs = subargs
+    else:
+        fn = level_1[subsubcommand]["fn"]
+        kwargs = subsubargs
 
     torch.set_float32_matmul_precision("high")
 
-    level_1 = subcommands_to_fn[subcommand]
-    if subsubcommand is None:
-        level_1(**subargs)
-    else:
-        level_2 = level_1[subsubcommand]
-        level_2(**subsubargs)
+    fn(**kwargs)
 
 
 if __name__ == "__main__":
