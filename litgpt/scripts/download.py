033e18111c5616991d7bd08b48478775e07c5ab6
rasbt <mail@sebastianraschka.com>
2024-03-19 21:03:01 +0000

    return model list in alphabetical order

--- litgpt/scripts/download.py
+++ litgpt/scripts/download.py
@@ -43,7 +43,7 @@ def download_from_hub(
 
         options = [f"{config['hf_config']['org']}/{config['hf_config']['name']}" for config in configs]
         print("Please specify --repo_id <repo_id>. Available values:")
-        print("\n".join(options))
+        print("\n".join(sorted(options)))
         return
 
     from huggingface_hub import snapshot_download
