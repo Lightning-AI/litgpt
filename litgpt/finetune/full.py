449eb29b111324090fa7066e0b26e9166806b02e
Sebastian Raschka <mail@sebastianraschka.com>
2024-04-01 15:44:48 -0500

    Reduce microbatch size (#1212)

--- litgpt/finetune/full.py
+++ litgpt/finetune/full.py
@@ -44,7 +44,7 @@ def setup(
     train: TrainArgs = TrainArgs(
         save_interval=1000,
         log_interval=1,
-        global_batch_size=64,
+        global_batch_size=16,
         micro_batch_size=1,
         lr_warmup_steps=100,
         epochs=5,
