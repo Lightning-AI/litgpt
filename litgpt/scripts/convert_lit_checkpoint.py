412e0fe6e523ace4cfc681ac3d12ab19914c4d9c
Sebastian Raschka <mail@sebastianraschka.com>
2024-04-16 06:13:13 -0400

    Typo fix in convert_lit_checkpoint.py (#1298)

--- litgpt/scripts/convert_lit_checkpoint.py
+++ litgpt/scripts/convert_lit_checkpoint.py
@@ -235,7 +235,7 @@ def check_conversion_supported(lit_weights: Dict[str, torch.Tensor]) -> None:
     if any("lora" in wn for wn in lit_weights):
         raise ValueError("Checkpoints with LoRA weights cannot be converted. Call `scripts/merge_lora.py` first.")
     if any("adapter" in wn or "gating_factor" in wn for wn in lit_weights):
-        raise NotImplementedError("Converting adapter models is supported.")
+        raise NotImplementedError("Converting adapter models is not supported.")
 
 
 @torch.inference_mode()
