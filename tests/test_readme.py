edcb52ccb36bc76ed8480aeac970781af62e5d17
rasbt <mail@sebastianraschka.com>
2024-04-17 15:47:04 +0000

    add finetuning

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,3 +1,5 @@
+# Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+
 from pathlib import Path
 import pytest
 import subprocess
@@ -25,3 +27,30 @@ def test_chat_with_model():
     prompt = "What do Llamas eat?"
     result = subprocess.run(command, input=prompt, text=True, capture_output=True, check=True)
     assert "What food do llamas eat?" in result.stdout
+
+
+@pytest.mark.dependency(depends=["test_download_model"])
+def test_finetune_model():
+
+    OUT_DIR = Path("out")/"lora"
+    DATASET_PATH = Path("custom_finetuning_dataset.json")
+    CHECKPOINT_DIR = f"checkpoints"/REPO_ID
+
+    download_command = ["curl", "-L", "https://huggingface.co/datasets/medalpaca/medical_meadow_health_advice/raw/main/medical_meadow_health_advice.json", "-o", str(DATASET_PATH)]
+    subprocess.run(download_command, check=True)
+
+    assert DATASET_PATH.exists(), "Dataset file not downloaded"
+
+    finetune_command = [
+        "litgpt", "finetune", "lora",
+        "--checkpoint_dir", str(CHECKPOINT_DIR),
+        "--data", "JSON",
+        "--data.json_path", str(DATASET_PATH),
+        "--data.val_split_fraction", "0.1",
+        "--train.max_steps", "1",
+        "--out_dir", str(OUT_DIR)
+    ]
+    run_command(finetune_command)
+
+    assert (OUT_DIR/"final").exists(), "Finetuning output directory was not created"
+    assert (OUT_DIR/"final"/"lit_model.pth").exists(), "Model file was not created"
