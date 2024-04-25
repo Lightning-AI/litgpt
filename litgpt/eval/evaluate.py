9044051545041a659a58d375861819648be2a5d7
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-04-25 14:19:30 -0400

    fix

--- litgpt/eval/evaluate.py
+++ litgpt/eval/evaluate.py
@@ -86,13 +86,20 @@ def convert_and_evaluate(
 
     copy_config_files(source_dir=checkpoint_dir, out_dir=out_dir)
 
-    model_path = out_dir / "model.pth"
+    model_path = out_dir / "pytorch_model.bin"
     if not model_path.exists() or force_conversion:
         convert_lit_checkpoint(checkpoint_dir=checkpoint_dir, output_dir=out_dir)
+    
+        # Hack: LitGPT's conversion doesn't save a pickle file that is compatible to be loaded with
+        # `torch.load(..., weights_only=True)`, which is a requirement in HFLM.
+        # So we're `torch.load`-ing and `torch.sav`-ing it again to work around this.
+        state_dict = torch.load(out_dir / "model.pth")
+        torch.save(state_dict, model_path)
+        os.remove(out_dir / "model.pth")
 
     from lm_eval.models.huggingface import HFLM
 
-    model = HFLM(pretrained=str(model_path), device=device, batch_size=batch_size, dtype=dtype)
+    model = HFLM(pretrained=str(out_dir), device=device, batch_size=batch_size, dtype=dtype)
 
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
