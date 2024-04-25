d4ebc32803f7f25d386ab2a45085866d71ffef44
belerico <belo.fede@outlook.com>
2024-04-25 21:27:00 +0200

    Always keep at least one token (the most probable one)

--- litgpt/generate/base.py
+++ litgpt/generate/base.py
@@ -35,9 +35,15 @@ def sample(
         logits = torch.full_like(logits, float("-inf")).scatter_(-1, i, v)
     # optionally crop the logits to smallest set of logits with a cumulative probability above top_p
     if top_p is not None:
-        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        sorted_logits, sorted_indices = torch.sort(logits, descending=False)
         cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
-        sorted_indices_to_remove = cumulative_probs > top_p
+        # Example:
+        # sorted_probs=[0.1, 0.15, 0.2, 0.25, 0.3] -> sorted_cumprobs=[0.1, 0.25, 0.45, 0.7, 1.0]
+        # sorted_indices_to_remove = [1, 1, 0, 0, 0] if top_p=0.7
+        sorted_indices_to_remove = cumulative_probs <= (1 - top_p)
+        # Keep at least 1 token always to prevent the case where no token is selected
+        # In this case the most probable one is always kept
+        sorted_indices_to_remove[-1:] = 0
         indices_to_remove = sorted_indices_to_remove.scatter(0, sorted_indices, sorted_indices_to_remove)
         logits = logits.masked_fill(indices_to_remove, float("-inf"))
     # optionally scale the logits and sample from a probability distribution
@@ -197,7 +203,7 @@ def main(
     L.seed_everything(1234)
     for i in range(num_samples):
         t0 = time.perf_counter()
-        y = generate(model, encoded, max_returned_tokens, temperature=temperature, top_k=top_k, top_p=top_p, eos_id=tokenizer.eos_id)
+        y = generate( model, encoded, max_returned_tokens, temperature=temperature, top_k=top_k, top_p=top_p, eos_id=tokenizer.eos_id)
         t = time.perf_counter() - t0
         for block in model.transformer.h:
             block.attn.kv_cache.reset_parameters()
