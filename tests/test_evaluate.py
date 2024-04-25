4137b3aada0824a30f69b11754350b57dbb74014
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-04-25 16:31:42 -0400

    update test

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -17,26 +17,36 @@ import yaml
 import litgpt.eval.evaluate as module
 from litgpt import GPT, Config
 from litgpt.scripts.download import download_from_hub
-from litgpt.scripts.convert_hf_checkpoint import convert_hf_checkpoint
 
 
-def test_evaluate_script(tmp_path):
-    download_from_hub(repo_id="EleutherAI/pythia-14m", checkpoint_dir=tmp_path)
-    checkpoint_dir = tmp_path / "EleutherAI" / "pythia-14m"
-    convert_hf_checkpoint(checkpoint_dir=checkpoint_dir)
-    
+@pytest.mark.xfail(
+    raises=(datasets.builder.DatasetGenerationError, NotImplementedError),
+    strict=False,
+    match="Loading a dataset cached in a LocalFileSystem is not supported",
+)
+def test_evaluate_script(tmp_path, monkeypatch):
+    ours_config = Config.from_name("pythia-14m")
+    download_from_hub(repo_id="EleutherAI/pythia-14m", tokenizer_only=True, checkpoint_dir=tmp_path)
+    shutil.move(str(tmp_path / "EleutherAI" / "pythia-14m" / "tokenizer.json"), str(tmp_path))
+    shutil.move(str(tmp_path / "EleutherAI" / "pythia-14m" / "tokenizer_config.json"), str(tmp_path))
+    ours_model = GPT(ours_config)
+    checkpoint_path = tmp_path / "lit_model.pth"
+    torch.save(ours_model.state_dict(), checkpoint_path)
+    config_path = tmp_path / "model_config.yaml"
+    with open(config_path, "w", encoding="utf-8") as fp:
+        yaml.dump(asdict(ours_config), fp)
+
+    fn_kwargs = dict(
+        checkpoint_dir=tmp_path,
+        out_dir=tmp_path / "out_dir",
+        device=None,
+        dtype=torch.float32,
+        limit=5,
+        tasks="mathqa"
+    )
     stdout = StringIO()
     with redirect_stdout(stdout), mock.patch("sys.argv", ["eval/evaluate.py"]):
-        module.convert_and_evaluate(
-            checkpoint_dir=checkpoint_dir,
-            out_dir=tmp_path / "out_dir",
-            device=None,
-            dtype=torch.float32,
-            limit=5,
-            tasks="mathqa"
-        )
-        
-    assert (tmp_path / "out_dir" / "results.json").is_file()
+        module.convert_and_evaluate(**fn_kwargs)
     stdout = stdout.getvalue()
     assert "mathqa" in stdout
     assert "Metric" in stdout
