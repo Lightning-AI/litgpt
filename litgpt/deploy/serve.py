708b05c26ab0e8dea61aa280959c70234be3f60c
rasbt <mail@sebastianraschka.com>
2024-05-20 20:33:35 +0000

    remove unnecessary import

--- litgpt/deploy/serve.py
+++ litgpt/deploy/serve.py
@@ -1,5 +1,4 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
-import json
 from pathlib import Path
 from typing import Dict, Any, Optional
 from litgpt.utils import check_valid_checkpoint_dir
