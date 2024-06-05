c1812ba8a6e02bb71a01efde329616c31da6f579
rasbt <mail@sebastianraschka.com>
2024-06-05 14:54:02 +0000

    fix copy paste issue

--- litgpt/finetune/adapter_v2.py
+++ litgpt/finetune/adapter_v2.py
@@ -212,7 +212,7 @@ def fit(
     data: DataModule,
 ) -> None:
     tokenizer = Tokenizer(checkpoint_dir)
-    longest_seq_length, longest_seq_ix = get_longest_seq_length(ConcatDataset([train_dataloader.dataset, val_dataloader.dataset]))h)
+    longest_seq_length, longest_seq_ix = get_longest_seq_length(ConcatDataset([train_dataloader.dataset, val_dataloader.dataset]))
     model.max_seq_length = min(longest_seq_length, train.max_seq_length or float("inf"))
     fabric.print(
         f"The longest sequence length in the train data is {longest_seq_length}, the model's maximum sequence length is"
