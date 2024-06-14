4d07631044f0d42f7485c2fb9d376a74a8f2e311
awaelchli <aedu.waelchli@gmail.com>
2024-06-14 20:20:02 +0200

    port changes to all scripts

--- litgpt/pretrain.py
+++ litgpt/pretrain.py
@@ -124,7 +124,7 @@ def setup(
     tokenizer = Tokenizer(tokenizer_dir) if tokenizer_dir is not None else None
 
     logger = choose_logger(
-        logger_name, out_dir, name=f"pretrain-{config.name}", resume=resume, log_interval=train.log_interval
+        logger_name, out_dir, name=f"pretrain-{config.name}", resume=bool(resume), log_interval=train.log_interval
     )
 
     if devices > 1:
