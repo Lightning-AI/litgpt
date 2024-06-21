b88d280688b1886d6db345aabc728753bb980e53
Aniket Maurya <theaniketmaurya@gmail.com>
2024-06-21 15:55:51 +0100

    add `LLM.generate` with streaming (#1511)
    Co-authored-by: Sebastian Raschka <mail@sebastianraschka.com>

--- litgpt/api.py
+++ litgpt/api.py
@@ -6,12 +6,12 @@ from typing import Any, List, Literal, Optional, Tuple, Union
 
 import torch
 import lightning as L
-from lightning.fabric.utilities.device_parser import _normalize_parse_gpu_input_to_list
 
 from litgpt.model import GPT  # needs to be imported before config
 from litgpt.config import Config
 from litgpt.tokenizer import Tokenizer
 from litgpt.generate.base import generate as generate_fn
+from litgpt.chat.base import generate as stream_generate_fn
 from litgpt.prompts import load_prompt_style, has_prompt_style, PromptStyle
 from litgpt.utils import (
     extend_checkpoint_dir,
@@ -36,6 +36,7 @@ class LLM:
         self.prompt_style = prompt_style
         self.checkpoint_dir = checkpoint_dir
         self.fabric = fabric
+
     """
     LLM model class for inference, pretraining, and finetuning.
 
@@ -46,6 +47,7 @@ class LLM:
         text = llm.generate("What do Llamas eat?", top_k=1)
         print(text)
     """
+
     @classmethod
     def load(
         cls,
@@ -144,7 +146,8 @@ class LLM:
         eos_id: Optional[int] = None,
         include_prompt: bool = True,
         return_as_token_ids: bool = False,
-    ) -> Tuple[str, torch.Tensor]:
+        stream: bool = False
+    ) -> Union[str, torch.Tensor]:
         """
         Takes a conditioning sequence (prompt) as input and continues to generate as many tokens as requested.
 
@@ -185,30 +188,53 @@ class LLM:
                 "Support for multiple devices is currently not implemented for `generate`"
             )
 
-        output_ids = generate_fn(
-            model=self.model,
-            prompt=input_ids.to(self.fabric.device),
-            max_returned_tokens=max_returned_tokens,
-            temperature=temperature,
-            top_k=top_k,
-            top_p=top_p,
-            eos_id=self.preprocessor.tokenizer.eos_id,
-            include_prompt=include_prompt,
-        )
+        def iterator():
+            outputs = stream_generate_fn(
+                model=self.model,
+                prompt=input_ids.to(self.fabric.device),
+                max_returned_tokens=max_returned_tokens,
+                temperature=temperature,
+                top_k=top_k,
+                top_p=top_p,
+                stop_tokens=([self.preprocessor.tokenizer.eos_id],),
+            )
+            if return_as_token_ids:
+                yield from outputs
+            else:
+                for output in outputs:
+                    yield self.preprocessor.tokenizer.decode(output)
+            return
+
+        if stream:
+            outputs = iterator()
+        else:
+            outputs = generate_fn(
+                model=self.model,
+                prompt=input_ids.to(self.fabric.device),
+                max_returned_tokens=max_returned_tokens,
+                temperature=temperature,
+                top_k=top_k,
+                top_p=top_p,
+                eos_id=self.preprocessor.tokenizer.eos_id,
+                include_prompt=include_prompt,
+            )
 
         for block in self.model.transformer.h:
             block.attn.kv_cache.reset_parameters()
 
-        if return_as_token_ids:
-            return output_ids
+        if stream:
+            return outputs
+        elif return_as_token_ids:
+            return outputs
         else:
-            return self.preprocessor.tokenizer.decode(output_ids)
+            return self.preprocessor.tokenizer.decode(outputs)
 
 
 class Preprocessor:
     """
     Preprocesser class for tokenization and de-tokenization.
     """
+
     def __init__(self, tokenizer: Tokenizer, device: str = "cpu") -> None:
         self.tokenizer = tokenizer
         self.device = device
