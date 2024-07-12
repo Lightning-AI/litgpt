3f08d2cc560edb196f906aca1407436505e19e72
rasbt <mail@sebastianraschka.com>
2024-07-12 16:01:11 +0000

    reorganize download and cpu-check

--- litgpt/chat/base.py
+++ litgpt/chat/base.py
@@ -20,7 +20,6 @@ from litgpt.scripts.merge_lora import merge_lora
 from litgpt.utils import (
     auto_download_checkpoint,
     check_file_size_on_cpu_and_warn,
-    check_valid_checkpoint_dir,
     get_default_supported_precision,
     load_checkpoint
 )
@@ -223,14 +222,14 @@ def main(
 
     fabric = L.Fabric(devices=1, precision=precision, plugins=plugins)
 
-    checkpoint_dir = auto_download_checkpoint(model_name=checkpoint_dir, access_token=access_token)
-    checkpoint_path = checkpoint_dir / "lit_model.pth"
-    check_file_size_on_cpu_and_warn(checkpoint_path, fabric.device)
-
     # Merge if this is a raw LoRA checkpoint
     if (checkpoint_dir / "lit_model.pth.lora").is_file() and not checkpoint_path.is_file():
         print("Merging LoRA weights with the base model. This won't take long and is a one-time-only thing.")
         merge_lora(checkpoint_dir)
+
+    checkpoint_dir = auto_download_checkpoint(model_name=checkpoint_dir, access_token=access_token)
+    checkpoint_path = checkpoint_dir / "lit_model.pth"
+    check_file_size_on_cpu_and_warn(checkpoint_path, fabric.device)
 
     config = Config.from_file(checkpoint_dir / "model_config.yaml")
 
