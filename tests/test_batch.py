39655b0d2c706564ce497988d0d4a2f1964550b2
apaz-cli <aarpazdera@gmail.com>
2024-09-03 15:09:39 -0400

    Added broken consistency test.

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -4,13 +4,39 @@ import warnings
 from pathlib import Path
 import lightning as L
 import litgpt
-from litgpt.generate.base import next_token, batched_next_token, batched_generate_fn
+from litgpt.generate.base import (
+    next_token,
+    batched_next_token,
+    batched_generate_fn,
+    generate_fn,
+)
 from litgpt.api import LLM, GPT
 from litgpt.scripts.download import download_from_hub
 from tests.conftest import RunIf
 
 
 warnings.filterwarnings("ignore")
+
+
+def create_llm(tmp_path, batch_size, max_seq_length, device) -> tuple[LLM, GPT]:
+
+    L.seed_everything(42)
+
+    model_name = "microsoft/phi-2"
+    download_from_hub(repo_id=model_name, tokenizer_only=True, checkpoint_dir=tmp_path)
+
+    llm: LLM = LLM.load(
+        model_name,
+        tokenizer_dir=Path(tmp_path / model_name),
+        init="random",
+    )
+    model: GPT = llm.model
+    model.set_kv_cache(
+        batch_size=batch_size, max_seq_length=max_seq_length, device=device
+    )
+
+    return llm, model
+
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="Test requires a GPU.")
 def test_batched_equivalence(tmp_path):
@@ -56,7 +82,9 @@ def test_batched_equivalence(tmp_path):
     model.clear_kv_cache()
     model.set_kv_cache(batch_size=batch_size, max_seq_length=50, device="cuda:0")
 
-    toks_1: torch.Tensor = batched_next_token(model, input_pos_1, batch_x1, sample_kwargs)
+    toks_1: torch.Tensor = batched_next_token(
+        model, input_pos_1, batch_x1, sample_kwargs
+    )
     toks_2: torch.Tensor = batched_next_token(model, input_pos_2, toks_1, sample_kwargs)
 
     assert toks_1.ndim == 2
@@ -109,42 +137,110 @@ def test_simple_batch():
     torch.testing.assert_close(outs1, outs1_ref)
     torch.backends.cuda.matmul.allow_tf32 = old_allow_tf32
 
+
 @RunIf(min_cuda_gpus=1)
-def test_batch_generate():
-
-    L.seed_everything(42)
-
-    tmp_path = Path("/tmp/temp_folder")
-    model_name = "microsoft/phi-2"
-    download_from_hub(repo_id=model_name, tokenizer_only=True, checkpoint_dir=tmp_path)
+def test_batch_generate(tmp_path):
 
     device = "cuda:0"
     batch_size = 3
     sample_kwargs = {"top_k": 1}
+    llm, model = create_llm(tmp_path, batch_size, 50, device)
 
-    llm: LLM = LLM.load(
-        model_name,
-        tokenizer_dir=Path(tmp_path / model_name),
-        init="random",
-    )
-    model: GPT = llm.model
-    model.set_kv_cache(batch_size=batch_size, max_seq_length=50, device=device)
-
-    x = torch.tensor(
-        [43993, 25, 1867, 466, 32660, 17485, 4483, 30, 198, 26410],
+    batch_x = torch.tensor(
+        [
+            [43993, 25, 1867, 466, 32660, 17485, 4483, 30, 198, 26410],
+            [25, 1867, 466, 32660, 17485, 4483, 30, 198, 26410, 7596],
+            [1867, 466, 32660, 17485, 4483, 30, 198, 26410, 7596, 7596],
+        ],
         device=device,
         dtype=torch.int64,
     )
 
-    batch_x = torch.stack([x] * batch_size, dim=0)
-
+    tokens = []
     for l in batched_generate_fn(
         model,
         prompts=batch_x,
         max_returned_tokens=50,
-        stop_tokens=[(42789,),], # llm.prompt_style.stop_tokens(llm.tokenizer),
+        stop_tokens=[
+            (42789,),
+            (23029,),
+            (7992,),
+        ],  # llm.prompt_style.stop_tokens(llm.tokenizer),
         sample_args=sample_kwargs,
         include_prompt=True,
         include_eos=False,
     ):
-        print("Outputed token:", l, "\n")
+        tokens.append([t.item() if t is not None else None for t in l])
+
+    first_stream = [t[0] for t in tokens if t[0] is not None]
+    assert len(first_stream) == 46
+    assert first_stream[-1] == 7596
+
+    second_stream = [t[1] for t in tokens if t[1] is not None]
+    assert len(second_stream) == 39
+    assert second_stream[-1] == 46964
+
+    third_stream = [t[2] for t in tokens if t[2] is not None]
+    assert len(third_stream) == 41
+    assert third_stream[-1] == 42358
+
+    # for t in llm.tokenizer.decode_stream([torch.tensor(i) for i in first_stream]):
+    #    print(t, end="", flush=True)
+    # print()
+
+
+@RunIf(min_cuda_gpus=1)
+def test_batch_generate_equivalence(tmp_path):
+
+    device = "cuda:0"
+    batch_size = 3
+    sample_kwargs = {"top_k": 1}
+    llm, model = create_llm(tmp_path, batch_size, 50, device)
+
+    batch_x = torch.tensor(
+        [
+            [43993, 25, 1867, 466, 32660, 17485, 4483, 30, 198, 26410],
+            [25, 1867, 466, 32660, 17485, 4483, 30, 198, 26410, 7596],
+            [1867, 466, 32660, 17485, 4483, 30, 198, 26410, 7596, 7596],
+        ],
+        device=device,
+        dtype=torch.int64,
+    )
+
+    batch_tokens = []
+    for l in batched_generate_fn(
+        model,
+        prompts=batch_x,
+        max_returned_tokens=50,
+        stop_tokens=[(33814,)],
+        sample_args=sample_kwargs,
+        include_prompt=True,
+        include_eos=False,
+    ):
+        batch_tokens.append([t.item() if t is not None else None for t in l])
+
+    first_stream = [t[0] for t in batch_tokens if t[0] is not None]
+
+    batch_size = 1
+    llm, model = create_llm(tmp_path, batch_size, 50, device)
+
+    tokens = []
+    for t in generate_fn(
+        model,
+        prompt=batch_x[0],
+        max_returned_tokens=50,
+        stop_tokens=[(33814,)],
+        include_prompt=True,
+        include_eos=False,
+        **sample_kwargs,
+    ):
+        if t.size(0) == 1:
+            tokens.append(t.item())
+        else:
+            tokens.extend(t.tolist())
+
+    # TODO: (apaz-cli) This consistency test doesn't actually work at the moment. It's inconsistent.
+    # The output is really close... Something is going on here. For the moment, maybe this is close enough?
+    # Enough at least that we can start prototyping.
+
+    # assert first_stream == tokens
