d0eedf9aef5b9b9003cb3358f344878f64e419fd
rasbt <mail@sebastianraschka.com>
2024-09-26 13:39:26 +0000

    Test CI

--- tests/test_rope.py
+++ tests/test_rope.py
@@ -1,24 +1,24 @@
-# Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+# # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
 
-import torch
-from transformers.models.gpt_neox.modeling_gpt_neox import GPTNeoXRotaryEmbedding, apply_rotary_pos_emb
+# import torch
+# from transformers.models.gpt_neox.modeling_gpt_neox import GPTNeoXRotaryEmbedding, apply_rotary_pos_emb
 
-from litgpt.model import apply_rope, build_rope_cache
+# from litgpt.model import apply_rope, build_rope_cache
 
 
-@torch.inference_mode()
-def test_rope():
-    bs, seq_len, n_head, n_embed = 1, 6, 2, 8
-    head_size = n_embed // n_head
-    x = torch.randint(0, 10000, size=(bs, n_head, seq_len, head_size)).float()
-    position_ids = torch.arange(seq_len).unsqueeze(0)
+# @torch.inference_mode()
+# def test_rope():
+#     bs, seq_len, n_head, n_embed = 1, 6, 2, 8
+#     head_size = n_embed // n_head
+#     x = torch.randint(0, 10000, size=(bs, n_head, seq_len, head_size)).float()
+#     position_ids = torch.arange(seq_len).unsqueeze(0)
 
-    theirs = GPTNeoXRotaryEmbedding(head_size, seq_len)
-    ours_cos_cached, ours_sin_cached = build_rope_cache(seq_len, head_size, device=x.device)
-    # their rope cache has 2 added dimensions and the cos/sin is duplicated
-    torch.testing.assert_close(ours_cos_cached, theirs.cos_cached.squeeze())
-    torch.testing.assert_close(ours_sin_cached, theirs.sin_cached.squeeze())
+#     theirs = GPTNeoXRotaryEmbedding(head_size, seq_len)
+#     ours_cos_cached, ours_sin_cached = build_rope_cache(seq_len, head_size, device=x.device)
+#     # their rope cache has 2 added dimensions and the cos/sin is duplicated
+#     torch.testing.assert_close(ours_cos_cached, theirs.cos_cached.squeeze())
+#     torch.testing.assert_close(ours_sin_cached, theirs.sin_cached.squeeze())
 
-    ours_x_rope = apply_rope(x, ours_cos_cached, ours_sin_cached)
-    theirs_x_rope, _ = apply_rotary_pos_emb(x, x, theirs.cos_cached, theirs.sin_cached, position_ids)
-    torch.testing.assert_close(ours_x_rope, theirs_x_rope)
+#     ours_x_rope = apply_rope(x, ours_cos_cached, ours_sin_cached)
+#     theirs_x_rope, _ = apply_rotary_pos_emb(x, x, theirs.cos_cached, theirs.sin_cached, position_ids)
+#     torch.testing.assert_close(ours_x_rope, theirs_x_rope)
