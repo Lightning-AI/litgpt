a76be27e890f991755d9e30de5c67251e6a3ae3b
Jirka B <j.borovec+github@gmail.com>
2025-03-12 07:51:47 +0100

    split

--- tests/ext_thunder/test_thunder_ddp.py
+++ tests/ext_thunder/test_thunder_ddp.py
@@ -19,22 +19,57 @@ def test_thunder_strategy_input_parsing():
         ThunderDDPStrategy(jit=False, executors=("python",))
 
 
+@_RunIf(min_cuda_gpus=2, standalone=True)
+@pytest.mark.parametrize("strategy", ["ddp", "fsdp"])
+def test_no_backward_sync(strategy):
+    fabric = Fabric(devices=2, accelerator="cuda", strategy=strategy)
+    fabric.launch()
+
+    # account for sharding in the case of FSDP
+    out_features = 1 if "ddp" in strategy else fabric.world_size
+    
+    model = torch.nn.Linear(1, out_features, bias=False, device=fabric.device)
+    x = torch.randn(1, 1, device=fabric.device)
+    model = fabric.setup(model)
+
+    # 6 iters, 3 grad accumulation iters
+    for i, enabled in enumerate((True, True, False, True, True, False), 1):
+        x = torch.tensor([i * (fabric.local_rank + 1)], device=fabric.device, dtype=torch.float32)
+
+        with fabric.no_backward_sync(model, enabled):
+            y = model(x)
+            fabric.backward(y.sum())
+        if not enabled:
+            # Math for the first 3 iters
+            #
+            # DistributedDataParallel
+            # (1*1+2*1+3*1 + 1*2+2*2+3*2) / 2       = 9
+            #  ^^^^^^^^^^^   ^^^^^^^^^^^  ^^^
+            #  rank0         rank1        allreduce
+            #
+            # thunder.distributed.ddp
+            # ((1*1+2*1) + (1*2+2*2)) / 2        + (3*1 + 3*2)  / 2        = 9
+            #   ^^^^^^^     ^^^^^^^   ^^^           ^^^   ^^^   ^^^
+            #   rank0       rank1     allreduce1    rank0 rank1 allreduce2
+            assert model.weight.grad.shape.numel() == 1, model.weight.grad.shape
+            assert model.weight.grad.item() == (9.0 if i == 3 else 22.5)
+            model.weight.grad = None
+
+
 @_RunIf(min_cuda_gpus=2, thunder=True, standalone=True)
-@pytest.mark.parametrize("choice", ["ddp", "thunder_ddp", "fsdp", "thunder_fsdp"])
-def test_no_backward_sync(choice):
-    if choice == "thunder_ddp":
+@pytest.mark.parametrize("choice", ["ddp", "fsdp"])
+def test_no_backward_sync_thunder(choice):
+    if choice == "ddp":
         strategy = ThunderDDPStrategy()
-    elif choice == "thunder_fsdp":
+    elif choice == "fsdp":
         strategy = ThunderFSDPStrategy()
-    else:
-        strategy = choice
 
     fabric = Fabric(devices=2, accelerator="cuda", strategy=strategy)
     fabric.launch()
 
     # account for sharding in the case of FSDP
     out_features = 1 if "ddp" in choice else fabric.world_size
-    
+
     model = torch.nn.Linear(1, out_features, bias=False, device=fabric.device)
     x = torch.randn(1, 1, device=fabric.device)
     model = fabric.setup(model)
@@ -62,7 +97,7 @@ def test_no_backward_sync(choice):
             assert model.weight.grad.item() == (9.0 if i == 3 else 22.5)
             assert not hasattr(model.weight, "_thunder_fsdp_unsharded_grad")
             model.weight.grad = None
-        elif choice == "thunder_fsdp":
+        elif choice == "fsdp":
             assert model.weight._thunder_fsdp_unsharded_grad.shape == (2, 1)
             assert model.weight.grad is None
 
